bcb81cbb7437345f6fa2ca1942df604d718b785d
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-10-28 15:28:57 +0000

    Forgot to commit Login model

--- application/config/config.php
+++ application/config/config.php
@@ -372,7 +372,7 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'files'; //'/var/chroot/users/a16_webapps_3/var/www'
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
