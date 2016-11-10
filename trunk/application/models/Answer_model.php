55527d36ccb435211550a01e9921d29f282c2dce
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-10 11:10:07 +0000

    Added file with initial structure.

---
+++ trunk/application/models/Answer_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Answer_model extends CI_Model {
+    
+    /**
+     * Adds the given answer of the resident to the database.
+     * 
+     * Members:
+     *      - residentID        (string)    The id of the resident that has entered the answer.
+     *      - questionID        (int)       The id of the question answered.
+     *      - answer            (int)       The entered answer.
+     *      - currentSession    (int)       The number of the session in progress, meaning that (currentSession-1) questionnaires are completed by the given resident.
+     */
+    function addAnswer($residentID, $questionID, $answer, $currentSession) {
+        
+    }
+}
