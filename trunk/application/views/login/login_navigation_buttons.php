30424eef1c98d1d4dc44ff0a500285556198374c
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-11 10:53:36 +0000

    Login page buttons now use bootstrap default

--- trunk/application/views/login/login_navigation_buttons.php
+++ trunk/application/views/login/login_navigation_buttons.php
@@ -1,7 +1,7 @@
 <form action=<?php echo base_url() . 'index.php/login/facial_recognition' ?> method="POST">
-	<input type="submit" name="login_facial_recognition" value="Log in using facial recognition">
+	<input class="btn btn-default" type="submit" name="login_facial_recognition" value="Log in using facial recognition">
 </form>
 
 <form action=<?php echo base_url() . 'index.php/login/manual' ?> method="POST">
-	<input type="submit" name="login_manual" value="Log in manually">
+	<input class="btn btn-default" type="submit" name="login_manual" value="Log in manually">
 </form>
