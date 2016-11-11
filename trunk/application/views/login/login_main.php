10a8596b60d830bd264509da0b85dd00eceaa201
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-11 11:18:40 +0000

    added nav view

--- trunk/application/views/login/login_main.php
+++ trunk/application/views/login/login_main.php
@@ -4,7 +4,6 @@
     
 	<head>
 		<meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -12,15 +11,7 @@
 		<title>Grace Age: Login</title>
 	</head>
 	<body>
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="#">
-                            Grace Age
-                        </a>
-                    </div>
-                </div>
-            </nav>
+            {navbar}
             <div class="container">
 		 <div class="jumbotron">
                 <p>
