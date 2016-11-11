5d9e664a4528c6ad15b6f209f9cf9ff92d44c74f
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-11 10:18:49 +0000

    Added bootstrap libraries simple styling test

--- trunk/application/views/login/login_main.php
+++ trunk/application/views/login/login_main.php
@@ -4,10 +4,15 @@
     
 	<head>
 		<meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<title>Grace Age: Login</title>
 	</head>
 	<body>
+            <div class="container">
 		<h1>Welcome to Grace Age</h1>
 		<p>
 			{feedback}	<!-- used to dispay error notifications, is often empty -->
@@ -18,6 +23,6 @@
 		<hr />
 
 		{navigation_buttons}
-
+            </div>
 	</body>
 </html>
