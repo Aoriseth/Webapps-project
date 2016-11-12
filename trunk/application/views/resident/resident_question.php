143511e6943ef30c4f5e6d08f158ee67a471e633
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-12 21:16:47 +0000

    seperate question and buttons, expand container

--- trunk/application/views/resident/resident_question.php
+++ trunk/application/views/resident/resident_question.php
@@ -1,70 +1,61 @@
-
-<?php 
-    $progress = 0;
+<?php
+$progress = 0;
 ?>
 <div class="container">
     <div class="row">
-               
         <div class="col-lg-12" >
             <div class="col-lg-1" ></div>
             <div class="col-lg-10" >
-                <p class="text-center">
-                    {question}
-                </p>
-                <?php foreach( $options as $option) { ?>
-                    <div class="col-lg-2" >
-                        <form action="<?php echo base_url().'index.php/resident/question?category='.$category.'&index='.($index+1) ?>" method="POST" class="text-center">
-                            <!--input class="glyphicon-user" type="submit" name="option" value="<?php echo htmlspecialchars( $option->option ) ?>"-->
-                            <button type="submit" name="category" value="<?php echo htmlspecialchars( $option->option ) ?>" >
-                            <span class="glyphicon glyphicon-edit"></span>
-                            
-                        </button></br>
-                        <?php echo htmlspecialchars( $option->option ) ?>
-                        </form>
-                    </div>
 
-                <?php } ?>
+                <div class="card" id="card_question">
+                    <p class="text-center">
+                        {question}
+                    </p>
+                </div>
+                
+                <div class="card" id="card_answer">
+                    <?php foreach ($options as $option) { ?>
+                        <div class="col-lg-2" >
+                            <form action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
+                                <!--input class="glyphicon-user" type="submit" name="option" value="<?php echo htmlspecialchars($option->option) ?>"-->
+                                <button type="submit" name="category" value="<?php echo htmlspecialchars($option->option) ?>" >
+                                    <span class="glyphicon glyphicon-edit"></span>
+                                </button></br>
+                                <?php echo htmlspecialchars($option->option) ?>
+                            </form>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
-            
+
             <div class="col-lg-1" ></div>
-        
-
         </div>
-        
-
     </div>
     <div class="row">
         <p class="text-center">
-                Question <?php echo $index+1 ?> of {category_size} from {category}.
-                </br>
-   
+            Question <?php echo $index + 1 ?> of {category_size} from {category}.
+            </br>
         </p>
-        
         <div class="progress">
-            
             <div class="progress-bar" role="progressbar" aria-valuenow="0"
-            aria-valuemin="0" aria-valuemax="100" 
-            style="width:<?php echo $index+1 ?>%">
+                 aria-valuemin="0" aria-valuemax="100" 
+                 style="width:<?php echo $index + 1 ?>%">
                 <span class="sr-only">70% Complete</span>
             </div>
         </div>
+        <?php if ($index > 0) { ?>
 
-
-        <?php if ( $index > 0 ) { ?>
-
-                <form action="<?php echo base_url().'index.php/resident/question?category='.$category.'&index='.($index-1) ?>" method="POST">
-                        <input class="btn btn-primary" type="submit" name="back" value="Go back">
-                </form>
-
+            <form action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index - 1) ?>" method="POST">
+                <input class="btn btn-primary" type="submit" name="back" value="Go back">
+            </form>
         <?php } ?>
     </div>
 </div>
 
 <script>
-
     function calculate_progress(category_size, question) {
 
-        document.write(question/category_size);
+        document.write(question / category_size);
 
-}
+    }
 </script>
