b6c57547030c65d14a548ea76850e28fed6c94cf
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-13 23:59:56 +0000

    change col-lg to col-md; relocate process-bar and move text onto it

--- trunk/application/views/resident/resident_question.php
+++ trunk/application/views/resident/resident_question.php
@@ -3,9 +3,9 @@ $progress = 0;
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-lg-12" >
-            <div class="col-lg-1" ></div>
-            <div class="col-lg-10">
+        <div class="col-md-12" >
+            <div class="col-md-1" ></div>
+            <div class="col-md-10">
 
                 <div class="card" id="card_question">
                     <p class="text-center">
@@ -16,7 +16,7 @@ $progress = 0;
                 <div class="card" id="card_answer">
                     <?php $emotion_index = 0;
                     foreach ($options as $option) { ?>
-                        <div class="col-lg-2" >
+                        <div class="col-md-2" >
                             <form class="button_form" action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
                                 <button class="button_emotion" type="submit" name="category" value="<?php echo htmlspecialchars($option->option) ?>"> 
                                     <img src=<?php echo base_url() . 'assets/imgs/emotions/' . $emotion_index . ".png" ?>>                                   
@@ -28,27 +28,28 @@ $progress = 0;
                 </div>
             </div>
 
-            <div class="col-lg-1" ></div>
+            <div class="col-md-1" ></div>
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-10">
-            <p class="text-center">
-                Question <?php echo $index + 1 ?> of {category_size} from {category}.
-                </br>
-            </p>
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" aria-valuenow="0"
-                     aria-valuemin="0" aria-valuemax="100" 
-                     style="width: <?php echo ( (int)$index / (int)$category_size * 100 )?>%">
-                    <!--span class="sr-only">70% Complete</span-->
+        <div class="col-md-12" >
+            <div class="col-md-10" col-md-offset-1>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="0"
+                         aria-valuemin="0" aria-valuemax="100" 
+                         style="width: <?php echo ( (int)$index / (int)$category_size * 100 )?>%">
+                                <!--span class="sr-only">70% Complete</span-->
+                        <p class="text-center">
+                            Question <?php echo $index + 1 ?> of {category_size} from {category}.
+                        </p>
+                    </div>
                 </div>
+                <?php if ($index > 0) { ?>
+                    <form action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index - 1) ?>" method="POST">
+                        <input class="btn btn-primary, col-md-1" type="submit" name="back" value="Go back">
+                    </form>
+                <?php } ?>
             </div>
-            <?php if ($index > 0) { ?>
-                <form action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index - 1) ?>" method="POST">
-                    <input class="btn btn-primary, col-lg-1" type="submit" name="back" value="Go back">
-                </form>
-            <?php } ?>
         </div>
     </div>
 </div>
