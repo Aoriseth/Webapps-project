9bd21580e1fa838dcf0688b3880fc2d3f1fc4fe5
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-13 22:43:45 +0000

    show process in progress-bar and align it with question card

--- application/views/resident/resident_question.php
+++ application/views/resident/resident_question.php
@@ -12,13 +12,14 @@ $progress = 0;
                         {question}
                     </p>
                 </div>
-                
+
                 <div class="card" id="card_answer">
-                    <?php $emotion_index = 0; foreach ($options as $option) { ?>
+                    <?php $emotion_index = 0;
+                    foreach ($options as $option) { ?>
                         <div class="col-lg-2" >
                             <form class="button_form" action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
                                 <button class="button_emotion" type="submit" name="category" value="<?php echo htmlspecialchars($option->option) ?>"> 
-                                        <img src=<?php echo base_url().'assets/imgs/emotions/'.$emotion_index.".png" ?>>                                   
+                                    <img src=<?php echo base_url() . 'assets/imgs/emotions/' . $emotion_index . ".png" ?>>                                   
                                 </button></br>
                                 <?php echo htmlspecialchars($option->option); $emotion_index++; ?>
                             </form>
@@ -31,30 +32,29 @@ $progress = 0;
         </div>
     </div>
     <div class="row">
-        <p class="text-center">
-            Question <?php echo $index + 1 ?> of {category_size} from {category}.
-            </br>
-        </p>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="0"
-                 aria-valuemin="0" aria-valuemax="100" 
-                 style="width:<?php echo $index + 1 ?>%">
-                <span class="sr-only">70% Complete</span>
+        <div class="col-lg-10" col-lg-offset-1>
+            <p class="text-center">
+                Question <?php echo $index + 1 ?> of {category_size} from {category}.
+                </br>
+            </p>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="0"
+                     aria-valuemin="0" aria-valuemax="100" 
+                     style="width:<?php echo $index / ${category_size} ?>%">
+                    <!--span class="sr-only">70% Complete</span-->
+                </div>
             </div>
+            <?php if ($index > 0) { ?>
+                <form action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index - 1) ?>" method="POST">
+                    <input class="btn btn-primary" type="submit" name="back" value="Go back">
+                </form>
+            <?php } ?>
         </div>
-        <?php if ($index > 0) { ?>
-
-            <form action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index - 1) ?>" method="POST">
-                <input class="btn btn-primary" type="submit" name="back" value="Go back">
-            </form>
-        <?php } ?>
     </div>
 </div>
 
 <script>
     function calculate_progress(category_size, question) {
-
         document.write(question / category_size);
-
     }
 </script>
