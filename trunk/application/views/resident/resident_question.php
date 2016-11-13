64abf3d50d876e99e76a3682e43f868c5b35cabb
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-13 22:57:42 +0000

    change operation to calculate process

--- trunk/application/views/resident/resident_question.php
+++ trunk/application/views/resident/resident_question.php
@@ -40,7 +40,7 @@ $progress = 0;
             <div class="progress">
                 <div class="progress-bar" role="progressbar" aria-valuenow="0"
                      aria-valuemin="0" aria-valuemax="100" 
-                     style="width:<?php echo $index / ${category_size} * 100?>%">
+                     style="width:<?php echo ( $index / (int)${category_size} * 100 )?>%">
                     <!--span class="sr-only">70% Complete</span-->
                 </div>
             </div>
