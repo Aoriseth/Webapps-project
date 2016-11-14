b0bfba700ade0f3126f2be6ce9f30b3d1d2080ae
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-14 10:05:30 +0000

    Adjusted functions for storing answer in database.

--- trunk/application/controllers/Resident.php
+++ trunk/application/controllers/Resident.php
@@ -70,7 +70,6 @@ class Resident extends CI_Controller {
 			 * form now posts the string value, use id instead? -> yes, please!
 			 */
                         //TODO: use real values
-                        log_message('info ', 'testing');
                         $currentSession = 0; //TODO: check resident's real current session and add 1.
                         $residentID = 'ep123';
                         $questionID = 10;
