1bdde2876b7cc988d82e858f402396e3e42e7139
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 16:08:42 +0000

    Seperate login selection buttons and actual login window

--- trunk/application/views/login/login_facial_recognition.php
+++ trunk/application/views/login/login_facial_recognition.php
@@ -1,6 +1,8 @@
-<p>
-	<i>Log in using facial recognition.</i>
-</p>
-<p>
-	&lt;TODO facial recognition system...&gt;
-</p>
+<div class="jumbotron">
+    <p>
+            <i>Log in using facial recognition.</i>
+    </p>
+    <p>
+            &lt;TODO facial recognition system...&gt;
+    </p>
+</div>
