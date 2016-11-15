7f5d1a08267ede43269879304116fbd68bb004de
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 15:53:22 +0000

    First bootstrap -> material design test

--- trunk/application/views/login/login_main.php
+++ trunk/application/views/login/login_main.php
@@ -6,9 +6,18 @@
 		<meta charset="UTF-8">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
+                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
+                <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+                 
 		<title>Grace Age: Login</title>
+                
+                <script>
+                        $.material.init();
+                </script>
 	</head>
 	<body>
             {navbar}
