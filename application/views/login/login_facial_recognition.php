eb9d480d825e535409fbdcc4660c22eef305a65e
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 19:04:14 +0000

    Facial Recognition login demo

--- application/views/login/login_facial_recognition.php
+++ application/views/login/login_facial_recognition.php
@@ -3,6 +3,35 @@
             <i>Log in using facial recognition.</i>
     </p>
     <p>
-            &lt;TODO facial recognition system...&gt;
+        <video style="background-color: black;" id="cam" width="100%" height="100%" autoplay>
+            
+        </video>
+        <script>
+                var video = document.getElementById('video');
+                if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                        video.src = window.URL.createObjectURL(stream);
+                        video.play();
+                    });
+                }
+
+                /* Legacy code below: getUserMedia 
+                else if(navigator.getUserMedia) { // Standard
+                    navigator.getUserMedia({ video: true }, function(stream) {
+                        video.src = stream;
+                        video.play();
+                    }, errBack);
+                } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+                    navigator.webkitGetUserMedia({ video: true }, function(stream){
+                        video.src = window.webkitURL.createObjectURL(stream);
+                        video.play();
+                    }, errBack);
+                } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+                    navigator.mozGetUserMedia({ video: true }, function(stream){
+                        video.src = window.URL.createObjectURL(stream);
+                        video.play();
+                    }, errBack);
+                }       
+        </script>
     </p>
 </div>
