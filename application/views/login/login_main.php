10ce0dc87c191816559718607e97f9b4f9915a4e
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 16:57:31 +0000

    Added comments to clarify loading material design

--- application/views/login/login_main.php
+++ application/views/login/login_main.php
@@ -4,20 +4,22 @@
     
 	<head>
 		<meta charset="UTF-8">
+                <!-- Load CSS -->
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
+                <!-- Load scripts -->
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
                 <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
                 <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+                 <!-- Activate Material Design -->
+                <script>$.material.init();</script>
                  
-		<title>Grace Age: Login</title>
-                
-                <script>
-                        $.material.init();
-                </script>
+		
+                <title>Grace Age: Login</title>
+               
 	</head>
 	<body>
             {navbar}
