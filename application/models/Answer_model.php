9d4412c8049d648d69df73160cdf14ff5706efa0
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 20:45:47 +0000

    Update answer instead of inserting a new one into the database when the resident goes back to a previous question.

--- application/models/Answer_model.php
+++ application/models/Answer_model.php
@@ -19,22 +19,37 @@ class Answer_model extends CI_Model {
      *      - currentSession    (int)       The number of the session in progress, meaning that (currentSession-1) questionnaires are completed by the given resident.
      */
     function storeAnswer($residentID, $questionID, $chosenOption, $currentSession) {
-        if(! hasQuestionAlreadyBeenAnswered($residentID, $questionID, $currentSession)) {
-			$answerData = array(
-				'elderly_id' => $residentID,
-				'question_id' => $questionID,
-				'answer' => $chosenOption,
-				'session' => $currentSession,
-				'datetime_answered' => date('Y-m-d H:i:s')
-			);
+		//Ready the array with the answers
+		$answerData = array(
+			'elderly_id' => $residentID,
+			'question_id' => $questionID,
+			'answer' => $chosenOption,
+			'session' => $currentSession,
+			'datetime_answered' => date('Y-m-d H:i:s'));
+		
+		//Check if the resident already answered this question (for instance by going back to the previous question).
+		$query_answers = $this->db->query("SELECT id FROM a16_webapps_3.answers WHERE elderly_id='$residentID' AND question_id='$questionID' AND session='$currentSession'");
+		if($query_answers->num_rows() == 0) {
 			$this->db->insert('a16_webapps_3.answers', $answerData);
 		} else {
-			
+			$whereArray = array(
+				'elderly_id' => $residentID,
+				'question_id' => $questionID,
+				'session' => $currentSession);
+			$this->db->where($whereArray);
+			$this->db->update('a16_webapps_3.answers', $answerData);
 		}
     }
 	
+	/**
+	 * Check if a record with an answer from a given resident in a given session exists
+	 * for a given question.
+	 */
 	private function hasQuestionAlreadyBeenAnswered($residentID, $questionID, $currentSession) {
-		
-		return FALSE;
+		$query_answers = $this->db->query("SELECT id FROM a16_webapps_3.answers WHERE elderly_id='$residentID' AND question_id='$questionID' AND session='$currentSession'");
+		if($query_answers->num_rows() == 0) {
+			return FALSE;
+		}
+		return TRUE;
 	}
 }
