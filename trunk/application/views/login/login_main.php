fc62c166f08b8153651225eee1ebaffcbdeddbc2
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 17:22:13 +0000

    Test with fixed navbar (possibly better for mobile) Removed some redundant css Added top padding for navbar

--- trunk/application/views/login/login_main.php
+++ trunk/application/views/login/login_main.php
@@ -10,6 +10,7 @@
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
+                
                 <!-- Load scripts -->
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
