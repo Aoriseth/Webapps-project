e4ced1d003f26675a7e2b863bbc3061dcc2c1d95
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 17:08:14 +0000

    Added basic viewport scaling

--- trunk/application/views/login/login_main.php
+++ trunk/application/views/login/login_main.php
@@ -4,6 +4,7 @@
     
 	<head>
 		<meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <!-- Load CSS -->
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
