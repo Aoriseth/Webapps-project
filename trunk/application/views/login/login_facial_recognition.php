f04eb2a1bc174f844eac2c51c9403c11669dbbd1
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-15 19:15:45 +0000

    Change camera system to getUserMedia navigator

--- trunk/application/views/login/login_facial_recognition.php
+++ trunk/application/views/login/login_facial_recognition.php
@@ -3,19 +3,21 @@
             <i>Log in using facial recognition.</i>
     </p>
     <p>
-        <video id="cam" width="100%" height="100%" autoplay>
+        <video id="camfr" width="100%" height="100%" autoplay="true">
             
         </video>
         <script>
-                var video = document.getElementById('cam');
-                if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-                        video.src = window.URL.createObjectURL(stream);
-                        video.play();
-                    });
-                }
-                alert("test");
-});      
+            var video = document.querySelector("#camfr");
+ 
+            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+            if (navigator.getUserMedia) {       
+                navigator.getUserMedia({video: true}, handleVideo, videoError);
+            }
+
+            function handleVideo(stream) {
+                video.src = window.URL.createObjectURL(stream);
+            }
         </script>
     </p>
 </div>
