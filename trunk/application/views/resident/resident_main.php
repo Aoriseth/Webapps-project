7969cb1bd90a467f708df419d423f354ff1a9ff6
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-17 19:25:45 +0000

    change <head> to material design

--- trunk/application/views/resident/resident_main.php
+++ trunk/application/views/resident/resident_main.php
@@ -1,32 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Grace Age: Resident</title>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-                <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-	        <link href=<?php echo base_url()."assets/css/main.css" ?> rel="stylesheet" type="text/css"/>
+    <head>
+        <meta charset="utf-8">
+        <title>Grace Age: Resident</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        </head>
-	<body>
-		{navbar}
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-2" >
-                            <form action=<?php echo base_url().'index.php/resident/gallery' ?> method="POST" class="text-center">
-                                <input class="btn btn-primary" type="submit" name="Gallery" value="View gallery" style="width: 100%">
-                            </form>
-                        </div>
-                        <div class="col-xs-8">
-                            <!--div class="jumbotron"-->       
-                                {content}		<!-- the main element of the page (login form etc) -->
-                            <!--/div-->
-                        </div>
-                        <div class="col-xs-2">
-                            {navigation_buttons}
-                        </div>
-                    </div>
-                    
+        <!-- Load CSS -->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+        <!-- Load scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+
+        <!-- Activate Material Design -->
+        <script>$.material.init();</script>
+
+    </head>
+    
+    <body>
+        {navbar}
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2" >
+                    <form action=<?php echo base_url() . 'index.php/resident/gallery' ?> method="POST" class="text-center">
+                        <input class="btn btn-primary" type="submit" name="Gallery" value="View gallery" style="width: 100%">
+                    </form>
                 </div>
-	</body>
+                <div class="col-xs-8">
+                    <!--div class="jumbotron"-->       
+                    {content}		<!-- the main element of the page (login form etc) -->
+                    <!--/div-->
+                </div>
+                <div class="col-xs-2">
+                    {navigation_buttons}
+                </div>
+            </div>
+
+        </div>
+    </body>
 </html>
