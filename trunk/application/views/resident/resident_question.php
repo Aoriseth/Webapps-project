4337ae7ee88cb044e1685bd455756ffd513b190b
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-17 15:11:20 +0000

    button: img -> background-image

--- trunk/application/views/resident/resident_question.php
+++ trunk/application/views/resident/resident_question.php
@@ -17,9 +17,11 @@ $progress = 0;
                     <?php $emotion_index = 0;
                     foreach ($options as $option) { ?>
                         <div class="col-xs-2" >
-                            <form class="button_form" action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
-                                <button class="btn btn-raised btn-default" id="button_emotion" type="submit" name="option" value="<?php echo htmlspecialchars($option->option) ?>"> 
-                                    <img src=<?php echo base_url() . 'assets/imgs/emotions/' . $emotion_index . ".png" ?> >                                   
+                            <form class="form_btn" action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
+                                <button class="btn btn-raised btn-default" id="button_emotion" 
+                                        type="submit" name="option" value="<?php echo htmlspecialchars($option->option) ?>"
+                                        style="background-image : url(<?php echo base_url() . 'assets/imgs/emotions/' . $emotion_index . ".png" ?>);"> 
+                                    <!--img src=<?php echo base_url() . 'assets/imgs/emotions/' . $emotion_index . ".png" ?> -->                                   
                                 </button></br>
                                 <?php echo htmlspecialchars($option->option); $emotion_index++; ?>
                             </form>
