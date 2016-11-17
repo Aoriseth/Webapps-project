6569f89d0c7f34c7c4333fcdfbdd15c488184765
r0442523 <r0442523@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-17 13:57:33 +0000

    fit image with btn-default

--- trunk/application/views/resident/resident_question.php
+++ trunk/application/views/resident/resident_question.php
@@ -17,7 +17,7 @@ $progress = 0;
                     <?php $emotion_index = 0;
                     foreach ($options as $option) { ?>
                         <div class="col-xs-2" >
-                            <form class="button_form" action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
+                            <form class="btn btn-raised btn-default" action="<?php echo base_url() . 'index.php/resident/question?category=' . $category . '&index=' . ($index + 1) ?>" method="POST" class="text-center">
                                 <button class="button_emotion" type="submit" name="option" value="<?php echo htmlspecialchars($option->option) ?>"> 
                                     <img src=<?php echo base_url() . 'assets/imgs/emotions/' . $emotion_index . ".png" ?> >                                   
                                 </button></br>
