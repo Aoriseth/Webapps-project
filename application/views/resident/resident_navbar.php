ca6094c373f77eec77b9d4ee235c434a8a76dc8a
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-18 11:01:22 +0000

    navbar added

--- application/views/resident/resident_navbar.php
+++ application/views/resident/resident_navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-fixed-top navbar-inverse">
+<nav class="navbar navbar-fixed-top navbar-inverse" style="height: 50px">
     <div class="container-fluid" >
         <div class="navbar-header">
             <a class="navbar-brand">
@@ -7,7 +7,7 @@
             
         </div>
         <ul>
-            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+            <span class="glyphicon glyphicon-user" aria-hidden="true" style="position: absolute" style="right: 1px"></span>
             </ul>
         
     </div>
