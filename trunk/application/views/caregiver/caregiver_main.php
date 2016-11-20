12437ab20a8671053a1207bd8723a691bc2ad941
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-20 10:38:04 +0000

    Solved small errors that prevented bootstrap from loading on the caregiver page

--- trunk/application/views/caregiver/caregiver_main.php
+++ trunk/application/views/caregiver/caregiver_main.php
@@ -7,9 +7,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Grace-Age : Caregiver Home</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
-		<link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
-		<link href="<?php echo base_url(); ?>assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
-		<link href="<?php echo base_url(); ?>assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css')?>" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
 <!--	HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--	 WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--	[if lt IE 9]>
