9db1b4d99ce1a1d5ac57da846bebc937248934bc
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-20 10:49:52 +0000

    Modified caregiver page to use material design Modified buttons to use raised profile and default color

--- trunk/application/views/caregiver/caregiver_navbar.php
+++ trunk/application/views/caregiver/caregiver_navbar.php
@@ -12,16 +12,16 @@
 </nav>
 
 <form action="overview" method="POST">
-    <input <?php if ( $page == 'overview' ) { ?>disabled<?php } ?> class="btn btn-primary" type="submit" name="resident" value="Resident overview">
+    <input <?php if ( $page == 'overview' ) { ?>disabled<?php } ?> class="btn btn-raised btn-default" type="submit" name="resident" value="Resident overview">
 	<?php if ( $page == 'overview' ) { echo ' &lt-- current page'; } ?>
 </form>
 
 <form action="groups" method="POST">
-    <input <?php if ( $page == 'groups' ) { ?>disabled<?php } ?> class="btn btn-primary" type="submit" name="groups" value="Group selection">
+    <input <?php if ( $page == 'groups' ) { ?>disabled<?php } ?> class="btn btn-raised btn-default" type="submit" name="groups" value="Group selection">
 	<?php if ( $page == 'groups' ) { echo ' &lt-- current page'; } ?>
 </form>
 
 <form action="statistics" method="POST">
-    <input <?php if ( $page == 'statistics' ) { ?>disabled<?php } ?> class="btn btn-primary" type="submit" name="statistics" value="Statistics">
+    <input <?php if ( $page == 'statistics' ) { ?>disabled<?php } ?> class="btn btn-raised btn-default" type="submit" name="statistics" value="Statistics">
 	<?php if ( $page == 'statistics' ) { echo ' &lt-- current page'; } ?>
 </form>
