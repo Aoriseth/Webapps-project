1102ad8ae24e8781fbe67ea10fb0d8b2229889ea
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-20 10:32:51 +0000

    disable caregiver navigation buttons on corresponding pages

--- application/views/caregiver/caregiver_navbar.php
+++ application/views/caregiver/caregiver_navbar.php
@@ -1,16 +1,18 @@
+
+
 <!-- I am currently on page '{page}' -->
 
 <form action="overview" method="POST">
-    <input class="btn btn-primary" type="submit" name="resident" value="Resident overview">
+    <input <?php if ( $page == 'overview' ) { ?>disabled<?php } ?> class="btn btn-primary" type="submit" name="resident" value="Resident overview">
 	<?php if ( $page == 'overview' ) { echo ' &lt-- current page'; } ?>
 </form>
 
 <form action="groups" method="POST">
-    <input class="btn btn-primary" type="submit" name="groups" value="Group selection">
+    <input <?php if ( $page == 'groups' ) { ?>disabled<?php } ?> class="btn btn-primary" type="submit" name="groups" value="Group selection">
 	<?php if ( $page == 'groups' ) { echo ' &lt-- current page'; } ?>
 </form>
 
 <form action="statistics" method="POST">
-    <input class="btn btn-primary" type="submit" name="statistics" value="Statistics">
+    <input <?php if ( $page == 'statistics' ) { ?>disabled<?php } ?> class="btn btn-primary" type="submit" name="statistics" value="Statistics">
 	<?php if ( $page == 'statistics' ) { echo ' &lt-- current page'; } ?>
 </form>
