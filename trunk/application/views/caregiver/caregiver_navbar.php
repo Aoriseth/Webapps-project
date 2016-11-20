76001e2e9660ae4d731ee499932de957d13220fe
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-20 12:09:54 +0000

    initial sidebar ordering for caregiver page

--- trunk/application/views/caregiver/caregiver_navbar.php
+++ trunk/application/views/caregiver/caregiver_navbar.php
@@ -5,23 +5,28 @@
 <nav class="navbar navbar-fixed-top navbar-inverse">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand">
                 Grace Age</a>
         </div>
     </div>
 </nav>
 
-<form action="overview" method="POST">
+
+
+
+<div class="well">
+    <form  action="overview" method="POST">
     <input <?php if ( $page == 'overview' ) { ?>disabled<?php } ?> class="btn btn-raised btn-default" type="submit" name="resident" value="Resident overview">
 	<?php if ( $page == 'overview' ) { echo ' &lt-- current page'; } ?>
 </form>
 
-<form action="groups" method="POST">
-    <input <?php if ( $page == 'groups' ) { ?>disabled<?php } ?> class="btn btn-raised btn-default" type="submit" name="groups" value="Group selection">
+    <form action="groups" method="POST">
+    <input <?php if ( $page == 'groups' ) { ?>disabled<?php } ?> class=" btn btn-raised btn-default" type="submit" name="groups" value="Group selection">
 	<?php if ( $page == 'groups' ) { echo ' &lt-- current page'; } ?>
 </form>
 
-<form action="statistics" method="POST">
-    <input <?php if ( $page == 'statistics' ) { ?>disabled<?php } ?> class="btn btn-raised btn-default" type="submit" name="statistics" value="Statistics">
+    <form  action="statistics" method="POST">
+    <input <?php if ( $page == 'statistics' ) { ?>disabled<?php } ?> class=" btn btn-raised btn-default" type="submit" name="statistics" value="Statistics">
 	<?php if ( $page == 'statistics' ) { echo ' &lt-- current page'; } ?>
 </form>
+</div>
