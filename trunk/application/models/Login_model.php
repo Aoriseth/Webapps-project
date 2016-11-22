d6af32f08238a2f3039a4be74d6919a398b766f3
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-22 19:33:01 +0000

    More improvements to login model.

--- trunk/application/models/Login_model.php
+++ trunk/application/models/Login_model.php
@@ -18,20 +18,10 @@ class Login_model extends CI_Model {
 	 *	- error = string
 	 */
 	function login( $username, $password ) {
-		/*
-		 * I think this can be optimized. We'll need an extra type 'field' in the database though.
-		 * 
-		 * SELECT id, password, first_name, type
-		 * FROM a16_webapps_3.residents FULL a16_webapps_3.caregivers
-		 * WHERE id=$username
-		 */
-		//$query_residents = $this->db->query( "SELECT * FROM a16_webapps_3.residents WHERE id='$username'" );
-		//$query_caregivers = $this->db->query( "SELECT * FROM a16_webapps_3.caregivers WHERE id='$username'" );
+		//Get all possible persons with the given username
 		$query = $this->db->query("SELECT id,type FROM a16_webapps_3.person_view WHERE id='$username'");
-		
 		$data[ 'succeeded' ] = false;
 		
-		//Alternative
 		//No matches
 		if ( $query->num_rows() == 0 ) {
 			$data[ 'error' ] = 'The username you\'ve entered does not exist in the system.';
@@ -40,44 +30,18 @@ class Login_model extends CI_Model {
 
 		//Too many matches
 		if ( $query->num_rows() > 1 ) {
-			$data[ 'error' ] = 'Found multiple entries with this username. Contact support please.';
+			$data[ 'error' ] = 'Found multiple entries with this username. Please contact support.';
 			return $data;
 		}
 		
-		//Get type of person
+		//Get this person's data from the database
 		$person = $query->row();
 		$data[ 'type' ] = $person->type;
-		if ( $data[ 'type' ] == 'resident' ) {
-			$query = $this->db->query("SELECT * FROM a16_webapps_3.residents WHERE id='$username'");
-		} else {
-			$query = $this->db->query("SELECT * FROM a16_webapps_3.caregivers WHERE id='$username'");
-		}
-		$person = $query->row();
-
-		/*
-		// no matches
-		if ( $query_residents->num_rows() == 0 && $query_caregivers->num_rows() == 0 ) {
-			$data[ 'error' ] = 'The username you\'ve entered does not exist in the system.';
-			return $data;
-		}
-
-		// too many matches?
-		if ( $query_residents->num_rows() + $query_caregivers->num_rows() > 1 ) {
-			$data[ 'error' ] = 'Found multiple entries with this username. Contact support please.';
-			return $data;
-		}
-
-		// get type of person
-		if ( $query_residents->num_rows() == 1 ) {
-			$data[ 'type' ] = 'resident';
-			$person = $query_residents->row();
-		} else {
-			$data[ 'type' ] = 'caregiver';
-			$person = $query_caregivers->row();
-		}
-		 */
+		$person = $this->db->query("SELECT * FROM a16_webapps_3."
+				. "$person->type"
+				. "s WHERE id='$username'")->row();
 		
-		// and as last verify password
+		//Verify password
 		if ( password_verify( $password, $person->password ) ) {
 			$data[ 'succeeded' ] = true;
 			$data[ 'name' ] = $person->first_name;
@@ -91,6 +55,5 @@ class Login_model extends CI_Model {
 			$data[ 'error' ] = 'Incorrect password.';
 		}
 		return $data;
- 
 	}
 }
