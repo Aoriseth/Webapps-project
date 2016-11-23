74c453f017a49e6d04bb49fb55d7248d7c58df1f
r0652164 <r0652164@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-23 19:53:49 +0000

    filtered the seen information for the list

--- trunk/application/models/Sort_model.php
+++ trunk/application/models/Sort_model.php
@@ -18,43 +18,23 @@ class Sort_model extends CI_Model {
 	}
 
 	private function _get_datatables_query()
-	{
-		
-		$this->db->from($this->table);
-
-		$i = 0;
-	
-                // loop column 
-		foreach ($this->column_search as $item) 
-		{
-                    // if datatable send POST for search
-			if($_POST['search']['value']) 
-			{
-				// first loop
-				if($i===0) 
-				{
-                                        // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
-					$this->db->group_start(); 
-					$this->db->like($item, $_POST['search']['value']);
-				}
-				else
-				{
+	{$this->db->from($this->table);	$i = 0;	                
+		foreach ($this->column_search as $item){ 
+                    if($_POST['search']['value']) 
+			{if($i===0){$this->db->group_start(); 
+                                    $this->db->like($item, $_POST['search']['value']);
+                                    }
+				else{
 					$this->db->or_like($item, $_POST['search']['value']);
 				}
-                                //last loop
-				if(count($this->column_search) - 1 == $i) 
-                                        //close bracket
+                                if(count($this->column_search) - 1 == $i)                                      
 					$this->db->group_end(); 
-			}
-			$i++;
-		}
-		// here order processing
-		if(isset($_POST['order'])) 
-		{
+			}$i++;
+		}		
+		if(isset($_POST['order'])){
 			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
 		} 
-		else if(isset($this->order))
-		{
+		else if(isset($this->order)){
 			$order = $this->order;
 			$this->db->order_by(key($order), $order[key($order)]);
 		}
