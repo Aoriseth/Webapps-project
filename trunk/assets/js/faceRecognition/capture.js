745fd75a912bcfe93700c8d4455c0399476b0e94
r0372268 <r0372268@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-23 18:42:04 +0000

    working on the live site because it fails because of the sessions error of CI/ php

--- trunk/assets/js/faceRecognition/capture.js
+++ trunk/assets/js/faceRecognition/capture.js
@@ -59,7 +59,8 @@
 
 function recognizePicture(dataURL) {
     getKeys(function (result) {
-        console.log(result2);
+        console.log(result);
+        console.log(result.responseText);
         var result2 = JSON.parse(result.responseText);
         
         var albumname = result.albumname;
