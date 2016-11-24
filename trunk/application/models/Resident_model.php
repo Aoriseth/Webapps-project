d81559640a58b1acc5c3cbcca03bfb69ab41c2b2
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-24 23:08:22 +0000

    Added queries for the residents.

--- trunk/application/models/Resident_model.php
+++ trunk/application/models/Resident_model.php
@@ -5,6 +5,7 @@ class Resident_model extends CI_Model {
 	public function __construct() {
 		parent::__construct();
 		
+		require 'lib/password.php';
 		$this->load->helper('date');
         date_default_timezone_set('Europe/Brussels');
 	}
@@ -32,6 +33,18 @@ class Resident_model extends CI_Model {
 	}
 	
 	/**
+	 * Get the resident with the given ID.
+	 */
+	function getResidentById( $residentID ) {
+		$query = $this->dB->query(
+			"SELECT * "
+			. "FROM a16_webapps_3.residents "
+			. "WHERE id='$residentID'"
+		);
+		return $query->result();
+	}
+	
+	/**
 	 * Update the last completed field with the current time and date
 	 * for a given resident (by ID).
 	 */
@@ -39,6 +52,83 @@ class Resident_model extends CI_Model {
 		$this->db->where('id', $residentID);
 		$this->db->set('last_completed', date('Y-m-d H:i:s'));
 		$this->db->update('a16_webapps_3.residents');
+	}
+	
+	/**
+	 * Insert a new resident into the database.
+	 */
+	function addResident( $residentID, $firstName, $lastName, $gender,
+			$password, $dateOfBirth, $language, $floorNumber,
+			$roomNumber, $lastDomicile, $caregiverID ) {
+		$array = array(
+			'id' => $residentID,
+			'first_name' => $firstName,
+			'last_name' => $lastName,
+			'gender' => $gender,
+			'password' => password_hash($password, PASSWORD_DEFAULT),
+			'date_of_birth' => $dateOfBirth,
+			'language' => $language,
+			'floor_number' => $floorNumber,
+			'room_number' => $roomNumber,
+			'last_domicile' => $lastDomicile,
+			'type' => 'resident',
+			'account_created_by' => $caregiverID,
+			'account_created_on' => date('Y-m-d H:i:s')
+		);
+		$this->db->insert('a16_webapps_3.residents', $array);
+	}
+	
+	/**
+	 * Get all residents that fit the requirements given in an array.
+	 * 
+	 * Example 1:
+	 * $array_requirements = array('floor_number' => 3)
+	 * 
+	 * Example 2:
+	 * $array_requirements = array('floor_number' => 3, 'gender' => 'Female')
+	 */
+	function getResidentsWith( $array_requirements ) {
+		$query = $this->db->get_where(
+				'a16_webapps_3.residents',
+				$array_requirements				
+		);
+		return $query->result();
+	}
+	
+	/*
+	 * Get all residents born before the given datetime.
+	 */
+	function getAllResidentsBornBefore( $datetime ) {
+		$query = $this->db->query(
+			"SELECT * "
+			. "FROM a16_webapps_3.residents "
+			. "WHERE date_of_birth < '$datetime'"
+		);
+		return $query->result();
+	}
+	
+	/*
+	 * Get all residents born after the given datetime.
+	 */
+	function getAllResidentsBornAfter( $datetime ) {
+		$query = $this->db->query(
+			"SELECT * "
+			. "FROM a16_webapps_3.residents "
+			. "WHERE date_of_birth > '$datetime'"
+		);
+		return $query->result();
+	}
+	
+	/*
+	 * Get all residents that were last active before the given datetime.
+	 */
+	function getAllResidentsLastActiveBefore( $datetime ) {
+		$query = $this->db->query(
+			"SELECT * "
+			. "FROM a16_webapps_3.residents "
+			. "WHERE last_activity < '$datetime'"
+		);
+		return $query->result();
 	}
 	
 	
