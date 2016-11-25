3986eedccaddaf1b114ed281f7176956557bc701
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-25 08:38:39 +0000

    colored Logout buttons red

--- trunk/application/views/resident/resident_navigation_buttons.php
+++ trunk/application/views/resident/resident_navigation_buttons.php
@@ -4,6 +4,6 @@
 </form>
 
 <form>
-    <button style="width: 100%" class=" btn-lg withripple btn btn-raised btn-default" formaction="<?php echo base_url() . 'index.php/logout' ?>">Logout</button>
+    <button style="width: 100%" class=" btn-lg withripple btn btn-raised btn-warning" formaction="<?php echo base_url() . 'index.php/logout' ?>">Logout</button>
 </form>
 
