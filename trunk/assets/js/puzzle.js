34f2f93ed08e72d5217fed2db545e10a6fe3f22e
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-26 22:47:47 +0000

    navbar added

---
+++ trunk/assets/js/puzzle.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    var image = new Image();
+    var greyImage = new Image();
+    var nrToDisplay = 3;
+    var nrDisplayed = 0;
+    image.src = 'http://localhost/www/trunk/assets/imgs/puzzle.jpg';
+    greyImage.src = 'http://localhost/www/trunk/assets/imgs/grey.jpg';
+    var canvas = document.getElementById("puzzle");
+    var context = canvas.getContext("2d");
+    var hRatio = canvas.width  / image.width    ;
+    var vRatio =  canvas.height / image.height  ;
+    var ratio  = Math.min ( hRatio, vRatio );
+    var centerShift_x = ( canvas.width - image.width*ratio ) / 2;
+    var centerShift_y = ( canvas.height - image.height*ratio ) / 2;
+    
+    for(var y = 0; y < 3; ++y) {
+        for(var x = 0; x < 3; ++x) {
+            if((Math.random() <= nrToDisplay/9 && nrDisplayed < nrToDisplay) || 9-3*(y+1)+x+1 <= nrToDisplay - nrDisplayed - 1){
+                context.drawImage(image, x * (image.width / 3), y * (image.height / 3), (image.width / 3), (image.height / 3),
+                centerShift_x +( x * image.width * ratio)/3, centerShift_y + (y * image.height * ratio)/3, (image.width * ratio)/3, (image.height * ratio)/3);
+                nrDisplayed++;
+            }else{
+                context.drawImage(greyImage,
+                centerShift_x +( x * image.width * ratio)/3, centerShift_y + (y * image.height * ratio)/3, (image.width * ratio)/3, (image.height * ratio)/3);
+            }
+        }
+    }
+};
