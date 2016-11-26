68162c75c66e8b9041472e98339721f7999bd17e
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-26 22:49:57 +0000

    puzzle added

--- trunk/application/views/resident/resident_home.php
+++ trunk/application/views/resident/resident_home.php
@@ -2,29 +2,16 @@
 <div class="container-fluid">
         
 
+   
+    <div class="row">
         <div class="well">
-            <h2 class="text-center">
-                Welcome {name}.<br>
-            </h2>
-			<?php if ( $display_login_notification == true ) { ?>
-				<!-- TODO replace this obvisously... -->
-				<p class="text-center">
-					I see you've just logged in...
-				</p>
-			<?php } ?>
             <form class="text-center">
                 <button style="width: 50%;font-size:2em;" class="btn-lg withripple btn btn-raised btn-info" formaction="<?php echo base_url() . 'index.php/resident/categories' ?>"><br>Start a new test!<br><br></button>
             </form>
-        </div>
-    
-        
-        <div class="well">
-            
-                <img src=<?php echo base_url().'assets/imgs/puzzle.jpg'?> alt="Puzzle View" style="width:100%;height:35vw;" class="center-block">
+                    
+            <canvas class="center-block img-responsive" id="puzzle" style="height: 35vw;width:content-box"></canvas>
         </div>
 
-            </div> 
-        
-    
+    </div> 
 
 </div>
