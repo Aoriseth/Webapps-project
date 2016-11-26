3f5678422044e76fe6834dc049c8f022e4da5dab
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-26 22:48:10 +0000

    navbar added

--- application/views/resident/resident_main.php
+++ application/views/resident/resident_main.php
@@ -18,6 +18,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/puzzle.js"></script>
 
         <!-- Activate Material Design -->
         <script>$.material.init();</script>
