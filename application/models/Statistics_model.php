89d526842d4ab95426bfdaf228ef4c4034d9ec4c
r0368818 <r0368818@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-26 16:49:56 +0000

    calculation for score add to graph. score for category is an average percentage of all the question answers (and all the residents)

---
+++ application/models/Statistics_model.php
@@ -0,0 +1,61 @@
+<?php
+
+class Statistics_model extends CI_Model{
+    
+    public function __construct() {
+		parent::__construct();
+		$this->load->model( 'Resident_model' );
+		
+	}
+    
+    function getWeightFor( $question_id ) {
+	return $this->Question_model->getQuestionsByID([$question_id])[0]->score_weight;
+    }  
+        
+    function getResidentAnswersFromCategory( $residentID, $categoryID) {
+		$query = $this->db->query(
+			"SELECT option_id, question_id"
+			. " FROM a16_webapps_3.answers"
+			. " WHERE resident_id='$residentID' AND category_id='$categoryID'"
+		);
+		return $query->result();
+    }
+    
+    function getScoreCategory($residentID, $categoryID) {
+
+        $answers = $this->getResidentAnswersFromCategory( $residentID, $categoryID);
+        $categoryScore = 0;
+        $categoryAverageScore = 0;
+
+        //for all questions
+            foreach ($answers as $answer) {
+                    $categoryScore += $answer->option_id/5*100*$this->getWeightFor($answer->question_id);
+            }
+        $categoryAverageScore = $categoryScore/count($answers);
+        return $categoryAverageScore;
+    }
+
+    function getAvarageScoreCategory($category) {
+        $totalScore = 0;
+        $residents = $this->Resident_model->getAllResidents();
+        //for all residents
+        foreach($residents as $resident){
+            $totalScore += getScoreCategory($resident->id, $category);
+        }
+
+        $avarageScore = $totalScore/count($residents);
+        return $avarageScore;
+
+
+    }
+
+    function getTotalScoreResident($resident, $categories){
+        $totalScore = 0;
+        //for all categories
+        foreach($categories as $category) {
+            $totalScore += getScoreCategory($resident, $category);
+        }
+
+        return $totalScore;
+    }
+}
