e92be8108a44b3d1e7461954b827d48e1bf704ee
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-28 23:59:10 +0000

    Improved upon initial model for storing pictures.

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -1,8 +1,27 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Picture_model extends CI_Model {
 
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	/**
+	 * Store the given picture in the database with the given resident as owner.
+	 */
+	function storePicture($residentID, $picture) {
+		//TODO this does not work! -> WIP
+		$imgData =addslashes(file_get_contents($_FILES['userImage']['tmp_name']));
+		$imageProperties = getimageSize($_FILES['userImage']['tmp_name']);
+		$this->db->query("INSERT INTO pictures(picture)
+		VALUES('{$imgData}')");
+	}
+	
+	/**
+	 * Get at most n pictures of the given resident, where n is a given
+	 * number of choice.
+	 */
+	function getNPictures($residentID, $n) {
+		
+	}
+}
