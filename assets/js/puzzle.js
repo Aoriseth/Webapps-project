c8b16813d029b23a5a5c1b31d3be7e9c4bbfcb43
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-29 13:24:01 +0000

    variable image path

--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -11,29 +11,30 @@ function loadPuzzle(baseURL) {
     image.src = baseURL.concat('/assets/imgs/puzzle.jpg');
     greyImage.src = baseURL.concat('/assets/imgs/grey.jpg');
     
-    console.log(image.src);
-    console.log(greyImage.src);
-    
-    var canvas = document.getElementById("puzzle");
-    var context = canvas.getContext("2d");
-    var hRatio = canvas.width  / image.width;
-    var vRatio =  canvas.height / image.height;
-    var ratio  = Math.min ( hRatio, vRatio );
-    var centerShift_x = ( canvas.width - image.width*ratio ) / 2;
-    var centerShift_y = ( canvas.height - image.height*ratio ) / 2;
-    
-    for(var y = 0; y < 3; ++y) {
-        for(var x = 0; x < 3; ++x) {
-            if((Math.random() <= nrToDisplay/9 && nrDisplayed < nrToDisplay) || (9-(3*y+x)) <= nrToDisplay - (nrDisplayed)){
-                context.drawImage(image, x * (image.width / 3), y * (image.height / 3), (image.width / 3), (image.height / 3),
-                centerShift_x +( x * image.width * ratio)/3, centerShift_y + (y * image.height * ratio)/3, (image.width * ratio)/3, (image.height * ratio)/3);
-                nrDisplayed++;
-                console.log("piece drawn" + y + x);
-            }else{
-                context.drawImage(greyImage,
-                centerShift_x +( x * image.width * ratio)/3, centerShift_y + (y * image.height * ratio)/3, (image.width * ratio)/3, (image.height * ratio)/3);
-                console.log("grey drawn "+ (9-(3*y+x)) + " " + nrToDisplay + " " + nrDisplayed);
+    greyImage.onload = function(){
+        var canvas = document.getElementById("puzzle");
+        var context = canvas.getContext("2d");
+        var hRatio = canvas.width  / image.width;
+        var vRatio =  canvas.height / image.height;
+        var ratio  = Math.min ( hRatio, vRatio );
+        var centerShift_x = ( canvas.width - image.width*ratio ) / 2;
+        var centerShift_y = ( canvas.height - image.height*ratio ) / 2;
+
+        for(var y = 0; y < 3; ++y) {
+            for(var x = 0; x < 3; ++x) {
+                if((Math.random() <= nrToDisplay/9 && nrDisplayed < nrToDisplay) || (9-(3*y+x)) <= nrToDisplay - (nrDisplayed)){
+                    context.drawImage(image, x * (image.width / 3), y * (image.height / 3), (image.width / 3), (image.height / 3),
+                    centerShift_x +( x * image.width * ratio)/3, centerShift_y + (y * image.height * ratio)/3, (image.width * ratio)/3, (image.height * ratio)/3);
+                    nrDisplayed++;
+                    console.log("piece drawn" + y + x);
+                }else{
+                    context.drawImage(greyImage,
+                    centerShift_x +( x * image.width * ratio)/3, centerShift_y + (y * image.height * ratio)/3, (image.width * ratio)/3, (image.height * ratio)/3);
+                    console.log("grey drawn "+ (9-(3*y+x)) + " " + nrToDisplay + " " + nrDisplayed);
+                }
             }
         }
     }
+    
+
 }
