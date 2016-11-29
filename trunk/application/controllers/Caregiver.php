d50bfdf182b89f783ebafb833284339fb3a0e2a9
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-29 22:49:21 +0000

    Testing: uploading pictures.

--- trunk/application/controllers/Caregiver.php
+++ trunk/application/controllers/Caregiver.php
@@ -116,51 +116,53 @@ class Caregiver extends CI_Controller {
                   
         }
 		
-		
+		/**
+		 * Upload a picture that is stored on the computer. Uploaded pictures are stored
+		 * in the upload folder.
+		 * 
+		 * |===============================|
+		 * | THIS IS A TEMPORARY FUNCTION. |
+		 * |===============================|
+		 * 
+		 * TODO: Move all of this code to the place where it is needed/appropriate for uploading pictures.
+		 */
 		function upload() {
 			$target_dir = "upload/";
 			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 			$uploadOk = 1;
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			
 			// Check if image file is a actual image or fake image
 			if(isset($_POST["submit"])) {
 				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-				if($check !== false) {
-					echo "File is an image - " . $check["mime"] . ".";
+				if($check != false) {
 					$uploadOk = 1;
-
 				} else {
-					echo "File is not an image."; 
 					$uploadOk = 0;
-
 				}
 			}
 			// Check if file already exists
 			if (file_exists($target_file)) {
-				echo "File already exists.";
+				//File already exists
 				$uploadOk = 0;
 			}
 			// Check file size
 			if ($_FILES["fileToUpload"]["size"] > 500000) {
-				echo "File is too large.";
+				//File is too large
 				$uploadOk = 0;
 			}
 			// Allow certain file formats
-			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "PNG" && $imageFileType != "jpeg"
-			&& $imageFileType != "gif" ) {
-				echo "Only JPG, JPEG, PNG & GIF files are allowed.";
+			if($imageFileType != "jpg" && $imageFileType != "JPG"
+					&& $imageFileType != "png" && $imageFileType != "PNG"
+					&& $imageFileType != "jpeg" && $imageFileType != "JPEG" ) {
+				//Something else than a jpg, JPG, png, PNG, jpeg, JPEG is give
 				$uploadOk = 0;
 			}
 			// Check if $uploadOk is set to 0 by an error
-			if ($uploadOk == 0) {
-				echo "File was not uploaded.";
-			// if everything is ok, try to upload file
-			} else {
+			if ($uploadOk != 0) {
 				if (is_uploaded_file($_FILES["fileToUpload"]["tmp_name"])) {
 					move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-					echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-				} else {
-					echo "There was an error uploading your file.";
+					//If this line is reached, the upload was successful
 				}
 			}
 		}
