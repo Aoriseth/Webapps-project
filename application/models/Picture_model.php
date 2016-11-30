4c61200044bacedef65a2a5291de89d55eb1ffb2
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 19:10:38 +0000

    Changed the way images are stored in database.

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -9,8 +9,9 @@ class Picture_model extends CI_Model {
 	/**
 	 * Store the given picture in the database with the given resident as owner.
 	 */
-	function storePicture($residentID, $picture_path) {
+	function storePicture($residentID, $picture_dir, $picture_name) {
 		$array = array(
+			'picture_dir' => addslashes($picutre_dir),
 			'picture_name' => addslashes($picture_path)
 		);
 		$this->db->insert('a16_webapps_3.pictures', $array);
