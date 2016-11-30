9b3dbbd7f98105cce1e3a40e9b2430f7d6dcfee8
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 19:37:28 +0000

    Category selection screen updated

--- trunk/application/views/resident/resident_categories.php
+++ trunk/application/views/resident/resident_categories.php
@@ -5,31 +5,32 @@
 
 
             <div class="col-lg-12" >
-                <p>
+                <h2>
                     Select a category:
-                </p>
+                </h2>
+                <hr/>
                 <?php foreach ($categories as $category) { ?>
                     <div class="col-xs-4" >
-                        <form action="<?php echo base_url() . 'index.php/resident/question' ?>" method="GET">
+                        <form style='text-align:center' action="<?php echo base_url() . 'index.php/resident/question' ?>" method="GET">
                             <!--input class="btn btn-default" type="submit" name="category" value="<?php echo $category->category ?>" class="text-center"-->
-                            <button type="submit" name="category" value="<?php echo $category->category ?>" class="text-center">
+                            <button type="submit" name="category" value="<?php echo $category->category ?>" class="btn btn-raised btn-default text-center">
                                 <?php if ($category->category === "Safety and security") { ?>
-                                    <span class="glyphicon glyphicon-facetime-video"></span>
+                                    <span style='font-size:10em' class="glyphicon glyphicon-facetime-video"></span>
                                 <?php } elseif ($category->category === "Food and meals") { ?>
-                                    <span class="glyphicon glyphicon-cutlery"></span> 
+                                    <span style='font-size:10em' class="glyphicon glyphicon-cutlery"></span> 
                                 <?php } elseif ($category->category === "Comfort") { ?>
-                                    <span class="glyphicon glyphicon-bed"></span>
+                                    <span style='font-size:10em' class="glyphicon glyphicon-bed"></span>
                                 <?php } elseif ($category->category === "Personal relationships") { ?>
-                                    <span class="glyphicon glyphicon-heart"></span> 
+                                    <span style='font-size:10em' class="glyphicon glyphicon-heart"></span> 
                                 <?php } elseif ($category->category === "Activities") { ?>
-                                    <span class="glyphicon glyphicon-blackboard"></span> 
+                                    <span style='font-size:10em' class="glyphicon glyphicon-blackboard"></span> 
                                 <?php } elseif ($category->category === "Privacy") { ?>
-                                    <span class="glyphicon glyphicon-home"></span> 
+                                    <span style='font-size:10em' class="glyphicon glyphicon-home"></span> 
                                 <?php } else { ?>
-                                    <span class="glyphicon glyphicon-edit"></span>
+                                    <span style='font-size:10em' class="glyphicon glyphicon-edit"></span>
                                 <?php } ?>
                             </button></br>
-                            <?php echo $category->category ?>
+                            <h3><?php echo $category->category ?></h3>
 
 
                         </form>
