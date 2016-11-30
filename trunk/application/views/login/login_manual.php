e307892afb174ab9078b8b9f1e9a75258f07937d
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 01:30:05 +0000

    Moved JS of login page to separate files (as much as seems reasonably possible).

--- trunk/application/views/login/login_manual.php
+++ trunk/application/views/login/login_manual.php
@@ -1,39 +1,17 @@
-<script type="text/javascript">
+<script src="<?php echo base_url(); ?>assets/js/login.js" type="text/javascript"></script>
+<script>
 $( function() {
 	$( "#login_form" ).submit( function( event ) {
 		var username = $( '#login_form input[ name = username ]').val();
 		var password = $( '#login_form input[ name = password ]').val();
 
-		$.ajax({
-			type: "POST",
-			url: "<?php echo base_url() ?>index.php/login/ajax", 
-			data: {
-				username: username,
-				password: password
-			},
-			dataType: "text",
-			cache: false,
-
-			success: function( data ) {
-				var response = JSON.parse( data );
-				if ( response[ 'success' ] ) {
-					// redirect to home page
-                                        // alert('Succesfully logged in');
-					window.location = '<?php base_url() ?>';
-				} else {
-					// display error message
-					$( '#error_message' ).html( response[ 'error' ] );
-					// return focus to username
-					$( '#login_form input[ name = username ]').focus();
-				}
-			}
-		});
+		login( '<?php echo base_url() ?>', username, password );
 		return false;
 	});
 });
 </script>
-<div id="test">
 
+<div>
 	<p>
 		Log in by manually typing your username and password.
 	</p>
