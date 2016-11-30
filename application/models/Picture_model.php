3351919129020676a37b4fff5bf6e5ccc63fc66a
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 13:54:03 +0000

    Adding picture name to database table.

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -9,8 +9,8 @@ class Picture_model extends CI_Model {
 	/**
 	 * Store the given picture in the database with the given resident as owner.
 	 */
-	function storePicture($residentID, $picture) {
-		
+	function storePicture($residentID, $picture_path) {
+		$this->db->insert('a16_webapps_3.pictures', $picture_path);
 	}
 	
 	/**
