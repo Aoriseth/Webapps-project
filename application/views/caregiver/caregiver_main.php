12e0bcb9fff392bbf1041c47ad2560ab5bade1da
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 20:23:44 +0000

    Caregiver updated jumbotron styling Caregiver navbar implementation

--- application/views/caregiver/caregiver_main.php
+++ application/views/caregiver/caregiver_main.php
@@ -1,57 +1,47 @@
 <!DOCTYPE html>
 
 <html lang="en">
-	<head> 
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Grace-Age : Caregiver Home</title>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link href="<?php echo base_url(); ?>assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
-		<link href="<?php echo base_url(); ?>assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
-                
-                <!-- Load scripts -->
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/statistics/chartCallbacks.js"></script>
-                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
-                
-                 <!-- Activate Material Design -->
-                <script>$.material.init();</script>
-<!--	HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!--	 WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--	[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+    <head> 
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Grace-Age : Caregiver Home</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="<?php echo base_url(); ?>assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
+
+        <!-- Load scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/statistics/chartCallbacks.js"></script>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
+
+        <!-- Activate Material Design -->
+        <script>$.material.init();</script>
+        <!--	HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--	 WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--	[if lt IE 9]>
+                          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+                          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+                        <![endif]-->
     </head> 
-	<body>
-		
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-sm-3 col-md-2 sidebar">
-                            <div class="well">
-                                {navbar}
-                                <hr>
-                                {navigation_buttons}
-                            </div>
-                        </div>
-                        
-                        <div class="col-sm-9 col-md-10">
-                            <div class="jumbotron">
-                                    {content}
-                            </div>
-                            <div class="col-sm-1 col-md-1">
-                                
-                            </div>
-                        </div>
+    <body>
+        {navbar}
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-offset-1 col-md-10">
+                    <div class="jumbotron">
+                        {content}
                     </div>
                 </div>
-	</body>
+            </div>
+        </div>
+    </body>
 </html>
