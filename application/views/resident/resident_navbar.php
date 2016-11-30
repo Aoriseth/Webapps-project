d8296fd78ee68999bb5ece1d66f5905e2f172950
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 19:13:16 +0000

    First implementation of resident navbar

--- application/views/resident/resident_navbar.php
+++ application/views/resident/resident_navbar.php
@@ -1,15 +1,36 @@
-<nav class="navbar navbar-fixed-top navbar-inverse" style="height: 50px">
-    <div class="container-fluid" >
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand">
-                Grace Age</a>
-            
-            
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Grace Age</a>
         </div>
-        <ul>
-            <span class="glyphicon glyphicon-user" aria-hidden="true" style="position: absolute" style="right: 1px"></span>
+        <div class="navbar-collapse collapse navbar-responsive-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo base_url() . 'index.php/resident/home' ?>">Home</a></li>
+                <li><a href="<?php echo base_url() . 'index.php/resident/gallery' ?>">Gallery</a></li>
             </ul>
-        
+            
+            <ul class="nav navbar-nav navbar-right">
+                    <li><a href="<?php echo base_url() . 'index.php/logout' ?>">LOGOUT</a></li>
+                    <li><a></a></li>
+                      
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+            </ul>
+        </div>
+            
+                
+
+        </div>
     </div>
-    
-</nav>
+
+
+
+<!--            <span class="glyphicon glyphicon-user" aria-hidden="true" style="position: absolute" style="right: 1px"></span>
+            </ul>-->
+
+
+
