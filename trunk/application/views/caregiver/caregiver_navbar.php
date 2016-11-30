e65ad5fa41e535584fe2f0c96804541c84a6e872
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 20:23:44 +0000

    Caregiver updated jumbotron styling Caregiver navbar implementation

--- trunk/application/views/caregiver/caregiver_navbar.php
+++ trunk/application/views/caregiver/caregiver_navbar.php
@@ -2,31 +2,35 @@
 
 <!-- I am currently on page '{page}' -->
 
-<nav class="navbar navbar-fixed-top navbar-inverse">
-    <div class="container">
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand">
-                Grace Age</a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Grace Age</a>
+        </div>
+        <div class="navbar-collapse collapse navbar-responsive-collapse">
+            <ul class="nav navbar-nav">
+                <li <?php if ( $page == 'home' ) { ?>class="active"<?php } ?> ><a href="<?php echo base_url() . 'index.php/caregiver/home' ?>">Home</a></li>
+                <li <?php if ( $page == 'overview' ) { ?>class="active"<?php } ?>><a href="<?php echo base_url() . 'index.php/caregiver/overview' ?>">Resident Overview</a></li>
+                <li <?php if ( $page == 'groups' ) { ?>class="active"<?php } ?>><a href="<?php echo base_url() . 'index.php/caregiver/groups' ?>">Group Selection</a></li>
+                <li <?php if ( $page == 'statistics' ) { ?>class="active"<?php } ?>><a href="<?php echo base_url() . 'index.php/caregiver/statistics' ?>">Statistics</a></li>
+            </ul>
+            
+            <ul class="nav navbar-nav navbar-right">
+                    <li><a href="<?php echo base_url() . 'index.php/logout' ?>">LOGOUT</a></li>
+            </ul>
+        </div>
+            
+                
+
         </div>
     </div>
-</nav>
 
 
 
 
-<div class="">
-    <form  action="overview" method="POST">
-    <input <?php if ( $page == 'overview' ) { ?>disabled<?php } ?> style="width: 100%" class="btn btn-raised btn-default" type="submit" name="resident" value="Resident overview">
-	
-</form>
 
-    <form action="groups" method="POST">
-    <input <?php if ( $page == 'groups' ) { ?>disabled<?php } ?> style="width: 100%" class=" btn btn-raised btn-default" type="submit" name="groups" value="Group selection">
-	
-</form>
-
-    <form  action="statistics" method="POST">
-    <input <?php if ( $page == 'statistics' ) { ?>disabled<?php } ?> style="width: 100%" class=" btn btn-raised btn-default" type="submit" name="statistics" value="Statistics">
-	
-</form>
-</div>
