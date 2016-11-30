590eef33a140e843f9880ad1c46e30970ebc2a19
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 20:00:43 +0000

    First version of welcome message caregiver

--- trunk/application/views/caregiver/caregiver_home.php
+++ trunk/application/views/caregiver/caregiver_home.php
@@ -3,40 +3,43 @@
 <hr />
 
 <p>
-	Hello {name}.
+    Hello {name}.
 </p>
 
-<?php if ( $display_login_notification == true ) { ?>
-	<!-- TODO replace this obvisously... -->
-	<p>
-		I see you've just logged in...
-	</p>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	Pretend you didn't see this. Just needed a place to test on the live site. After these tests are done, all of this can be removed.
-	<form action=<?php echo base_url().'index.php/caregiver/upload' ?> method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-	</form>
+<?php if ($display_login_notification == true) { ?>
+    <!-- TODO replace this obvisously... -->
+    <script type="text/javascript">
+        $(document).ready(function () {
+                $.snackbar({content: 'Hello {name}, you succesfully logged in!'});
+        });
+    </script>
+    
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    Pretend you didn't see this. Just needed a place to test on the live site. After these tests are done, all of this can be removed.
+    <form action=<?php echo base_url() . 'index.php/caregiver/upload' ?> method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+    </form>
 <?php } ?>
