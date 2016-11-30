11a024c29f695e7abb9bfd81113df0e0bec4a5f4
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 19:29:30 +0000

    Uploading picture to server operational. Inserting/fetching picture path+name from database operational.

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -7,14 +7,31 @@ class Picture_model extends CI_Model {
 	}
 	
 	/**
-	 * Store the given picture in the database with the given resident as owner.
+	 * Store the given picture in the database.
 	 */
-	function storePicture($residentID, $picture_dir, $picture_name) {
+	function storePicture($picture_dir, $picture_name) {
 		$array = array(
 			'picture_dir' => addslashes($picture_dir),
 			'picture_name' => addslashes($picture_name)
 		);
 		$this->db->insert('a16_webapps_3.pictures', $array);
+	}
+	
+	/**
+	 * Return a picture stored in the database. Both the folder where it is
+	 * stored in (picture_dir) and the picture name are returned.
+	 * 
+	 * The full path of the picture can be get as follows:
+	 * $full_path = $picutre_dir . $picture_name
+	 * 
+	 */
+	function getPicture($pictureID) {
+		$query = $this->db->query(
+			"SELECT picture_dir, picture_name"
+			. " FROM a16_webapps_3.pictures"
+			. " WHERE id='$pictureID'"
+		);
+		return $query->result();
 	}
 	
 	/**
