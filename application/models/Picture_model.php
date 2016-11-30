8868ddce11e9d20f866f7e3cffbc2bf320fe600a
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 20:14:58 +0000

    Debugging: retreiving picture id after insert.

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -24,7 +24,7 @@ class Picture_model extends CI_Model {
 			'picture_name' => addslashes($picture_name)
 		);
 		$this->db->insert('a16_webapps_3.pictures', $array);
-		echo $this->conn_id->insert_id();
+		echo $this->insert_id();
 		exit;
 	}
 	
