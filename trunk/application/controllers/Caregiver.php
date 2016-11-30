6a8fd7e778648ced85ac9a2c1e29dd0b2019e461
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-11-30 14:35:53 +0000

    Debugging

--- trunk/application/controllers/Caregiver.php
+++ trunk/application/controllers/Caregiver.php
@@ -177,8 +177,6 @@ class Caregiver extends CI_Controller {
 					//If this line is reached, the upload was successful
 					echo 'Picture uploaded! ';
 					echo $target_file;
-					echo $target_file['name'];
-					exit;
 					$this->Picture_model->storePicture(1, $target_file);
 				}
 				else {
