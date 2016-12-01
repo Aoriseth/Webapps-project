0411d094b5b44ed7e70bd8a205efa6bcf4ad5379
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-01 19:48:19 +0000

    improved scaling resident home

--- trunk/application/views/resident/resident_home.php
+++ trunk/application/views/resident/resident_home.php
@@ -1,26 +1,24 @@
 
-<div class="container-fluid">
-        
-   
-    <div class="row">
-        <div class="jumbotron">
-            <form class="text-center">
-                <button style="width: 50%;font-size:2em;" class="btn-lg withripple btn btn-raised btn-info" formaction="<?php echo base_url() . 'index.php/resident/categories' ?>"><br>Start a new test!<br><br></button>
-            </form>
-                    
-            <canvas class="center-block img-responsive" id="puzzle" style="height: 35vw;width:content-box"></canvas>
-            <script> loadPuzzle("<?php echo base_url() ?>"); </script>
-            
+
+
+    
+        <div class="well">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3"
+                     <form>
+                        <button style="width:100%;font-size:2.5vmax;" class="btn-lg withripple btn btn-raised btn-info" formaction="<?php echo base_url() . 'index.php/resident/categories' ?>">Start a new test!</button>
+                    </form>
+                </div>
+            </div>
+            <canvas class="center-block img-responsive" id="puzzle" style="height: 35vw;width:auto"></canvas>
+            <script> loadPuzzle("<?php echo base_url() ?>");</script>
+
         </div>
 
-    </div> 
-
-</div>
 <?php if ($display_login_notification == true) { ?>
-    <!-- TODO replace this obvisously... -->
     <script type="text/javascript">
         $(document).ready(function () {
-                $.snackbar({content: 'Hello {name}, you succesfully logged in!'});
+            $.snackbar({content: 'Hello {name}, you succesfully logged in!'});
         });
     </script>
 <?php } ?>
