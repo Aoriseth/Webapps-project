4391d8040adda84c375d8db9474118db64285449
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-01 20:58:43 +0000

    Cleaned up resident main by moving css and js lines to include

--- application/views/resident/resident_main.php
+++ application/views/resident/resident_main.php
@@ -3,27 +3,11 @@
     <head>
         <meta charset="utf-8">
         <title>Grace Age: Resident</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Load CSS -->
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
+       {include}
 
         <!-- Load scripts -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
-        <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+        
         <script src="<?php echo base_url(); ?>assets/js/puzzle.js"></script>
-        <script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
-
-        <!-- Activate Material Design -->
-        <script>$.material.init();</script>
 
     </head>
     
@@ -41,7 +25,8 @@
                 </div>
             </div>
             
-            <span data-toggle=snackbar data-content="This is my awesome snackbar!"></span>
         </div>
+        
+        <!--<a class="btn btn-warning btn-raised" style="height:3em;width:7em;position:fixed;bottom:30px;right:30px;" href="#">HELP</a>-->
     </body>
 </html>
