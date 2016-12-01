bb8d4b38045a6788c895a61ef832aeec05e43d0c
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-01 20:58:57 +0000

    Updated include to actually work

--- application/views/include.php
+++ application/views/include.php
@@ -6,6 +6,8 @@
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
                 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
+                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
+                <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
                 
                 <!-- Load scripts -->
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -13,6 +15,7 @@
                 <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
                 <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
                 <script src="<?php echo base_url(); ?>assets/js/faceRecognition/FRLambdaAPI.js"></script>
+                <script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
                  <!-- Activate Material Design -->
                 <script>$.material.init();</script>
                  
