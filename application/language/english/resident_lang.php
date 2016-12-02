f59262bbccbf381259734d00f944aa691485fcb9
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 02:10:10 +0000

    Language support on resident/gallery.

--- application/language/english/resident_lang.php
+++ application/language/english/resident_lang.php
@@ -7,3 +7,6 @@ $lang[ 'resident_button_test' ] = 'Start a new test!';
 $lang[ 'resident_button_test_help' ] = 'Click on \'Start a new test!\' to start giving feedback!';
 
 $lang[ 'resident_puzzle_help' ] = '<-- You can see your progress on the puzzle here.';
+
+// gallery
+$lang[ 'resident_gallery_explanation' ] = 'Your finished puzzles will be displaeyd here.';
