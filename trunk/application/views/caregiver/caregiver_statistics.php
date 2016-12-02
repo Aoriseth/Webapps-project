811129c71a0cc1d5cff11bc36b378dd5e7591a74
r0368818 <r0368818@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 14:30:35 +0000

    more charts

--- trunk/application/views/caregiver/caregiver_statistics.php
+++ trunk/application/views/caregiver/caregiver_statistics.php
@@ -8,58 +8,123 @@
 <body>
     <!--Div that will hold the pie chart-->
     
-    <div id="chart_div"></div>
+    <div id="chart1_div"></div>
     
-    <form method="POST" id="select_axis_form" name="selectForm">
+    <form method="POST" id="chart1_form" name="chart1Form">
+
+        <select name="residents" id="residents_select">
+            <?php foreach ($residents as $resident){ ?>   
+                   <option value=<?php echo json_encode($resident->id); ?>> <?php echo json_encode($resident->first_name); ?> </option>
+            <?php } ?>
+                       
+        </select>
+        <select name="language" id="language_select" >
+              
+                        <option value="English"> "English" </option>
+                        <option value="Nederlands"> "Nederlands" </option>
+                       
+        </select>
+        <input type="submit" value="submit chart1" name="submit chart1"/>
+    </form>
+    
+    <div id="chart2_div"></div>
+    
+    <form method="POST" id="chart2_form" name="chart2Form">
         <select name="categories" id="categories_select">
             <?php foreach ($categories as $category){ ?>   
                         <option value=<?php echo json_encode($category->id); ?>> <?php echo json_encode($category->category); ?> </option>
             <?php } ?>           
         </select>
-        <select name="residents" id="residents_select">
-            <?php foreach ($residents as $resident){ ?>   
-                        <option value=<?php echo json_encode($resident->id); ?>> <?php echo json_encode($resident->first_name); ?> </option>
-            <?php } ?>           
+
+        <select name="language" id="language_select" >
+              
+                        <option value="English"> "English" </option>
+                        <option value="Nederlands"> "Nederlands" </option>
+                       
         </select>
-        <input type="submit" value="submit" name="submit"/>
+        <input type="submit" value="submit chart2" name="submit chart2"/>
     </form>
+    
+    
 <script type="text/javascript">
-        var formAxis = document.getElementById('select_axis_form');
+        var formChart1 = document.getElementById('chart1_form');
+        var formChart2 = document.getElementById('chart2_form');
         
-        formAxis.addEventListener('submit', function(e) {
+        formChart1.addEventListener('submit', function(e) {
             e.preventDefault();
             var selects = this.getElementsByTagName('select');
-            var category = parseInt(selects[0].value);
-            var resident = selects[1].value;
+            var resident = selects[0].value;
+            var language = selects[1].value;
+
             
             $.ajax({
                 type: "POST",
                 url: "<?php echo base_url() ?>index.php/caregiver/load_charts", 
-                data: {category: category, resident: resident},
+                data: { resident: resident, language: language},
                 dataType: "text",
                 cache: false,
 
                 success: function( data ) {
-                    var categories = [];
+                    var Yaxis = [];
 
-                    var scores = [];
+                    var Xaxis = [];
                     console.log(data);
                     var response = JSON.parse( data);
                     console.log(response);
                     
-                    for(var i = 0; i < response[0].length; i++){
-                        categories.push(response[0][i].category);
-                    }
-                    for(var i = 1; i < response.length; i++){
-                        scores.push(response[i]);
-                    }
+                    Xaxis = response[0][0];
+                    Yaxis = response[0][1];
+
                     google.charts.load('current', {'packages':['corechart']});
                     google.charts.setOnLoadCallback(drawChart);
+
                     
                     function drawChart() {
 
                         var data = new google.visualization.DataTable();
-                        dataChart(categories, scores, data, "chart_div", "resident");
+                        dataChart(Yaxis, Xaxis, data, "chart1_div", resident);
+                    }
+
+                    
+                }
+            });
+            return false;
+        });
+        
+        formChart2.addEventListener('submit', function(e) {
+            e.preventDefault();
+            var selects = this.getElementsByTagName('select');
+
+            
+            var category = parseInt(selects[0].value);
+            var language = selects[1].value;
+            
+            $.ajax({
+                type: "POST",
+                url: "<?php echo base_url() ?>index.php/caregiver/load_charts", 
+                data: { language: language, category: category},
+                dataType: "text",
+                cache: false,
+
+                success: function( data ) {
+                    var Yaxis = [];
+
+                    var Xaxis = [];
+                    console.log(data);
+                    var response = JSON.parse( data);
+                    console.log(response);
+                    
+                    Xaxis = response[1][0];
+                    Yaxis = response[1][1];
+
+                    google.charts.load('current', {'packages':['corechart']});
+                    google.charts.setOnLoadCallback(drawChart2);
+                    
+
+                    function drawChart2() {
+
+                        var data = new google.visualization.DataTable();
+                        dataChart(Yaxis, Xaxis, data, "chart2_div", selects[0].value);
                     }
                     
                 }
