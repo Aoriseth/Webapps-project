668b43614f13600cbb899b77e63b338e3ffe7a17
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 01:59:45 +0000

    Implemented language support on resident_home.

--- application/language/english/resident_lang.php
+++ application/language/english/resident_lang.php
@@ -1,1 +1,9 @@
 <?php
+
+// home
+$lang[ 'resident_welcome_snackbar' ] = 'Hello {name}, you succesfully logged in!';
+
+$lang[ 'resident_button_test' ] = 'Start a new test!';
+$lang[ 'resident_button_test_help' ] = 'Click on \'Start a new test!\' to start giving feedback!';
+
+$lang[ 'resident_puzzle_help' ] = '<-- You can see your progress on the puzzle here.';
--- application/language/nederlands/resident_lang.php
+++ application/language/nederlands/resident_lang.php
@@ -1,1 +1,9 @@
 <?php
+
+// home
+$lang[ 'resident_welcome_snackbar' ] = 'Hallo {name}, je bent correct aangemald!';
+
+$lang[ 'resident_button_test' ] = 'Start een nieuwe test!';
+$lang[ 'resident_button_test_help' ] = 'Klik op \'Start een nieuwe test\' om feedback te geven!';
+
+$lang[ 'resident_puzzle_help' ] = '<-- Je kunt je vooruitgang zien op de puzzel hier.';
