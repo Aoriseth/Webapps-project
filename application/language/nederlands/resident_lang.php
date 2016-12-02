1153a77e8653adc35a5c4da09d2e8135d5fa9225
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 08:32:53 +0000

    Fixed some more small typos

--- application/language/nederlands/resident_lang.php
+++ application/language/nederlands/resident_lang.php
@@ -9,12 +9,12 @@ $lang[ 'resident_navbar_gallery' ] = 'Galerij';
 $lang[ 'resident_navbar_gallery_help' ] = 'Klik op \'galerij\' om de afgwerkte puzzels te zien.';
 
 // home
-$lang[ 'resident_welcome_snackbar' ] = 'Hallo {name}, je bent correct aangemald.';
+$lang[ 'resident_welcome_snackbar' ] = 'Hallo {name}, je bent correct aangemeld.';
 
 $lang[ 'resident_button_test' ] = 'Start een nieuwe test!';
 $lang[ 'resident_button_test_help' ] = 'Klik op \'Start een nieuwe test\' om feedback te geven.';
 
-$lang[ 'resident_puzzle_help' ] = '<-- Je kunt je vooruitgang zien op de puzzel hier.';
+$lang[ 'resident_puzzle_help' ] = 'Je kunt je vooruitgang zien op de puzzel hier.';
 
 // gallery
 $lang[ 'resident_gallery_explanation' ] = 'Hier komen je afgewerkte puzzels te staan.';
