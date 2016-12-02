1b0279cb0cc560975b8ab3ae98e332e484ec9de5
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 15:03:49 +0000

    Fixed button for new test

--- application/views/resident/resident_home.php
+++ application/views/resident/resident_home.php
@@ -1,30 +1,35 @@
 <script>
-	$(document).ready(function () {
-		$('#popover').popover({
-			content: 'Popover content',
-			trigger: 'manual'
-		});
-	});
+    $(document).ready(function () {
+        $('#popover').popover({
+            content: 'Popover content',
+            trigger: 'manual'
+        });
+    });
 </script>
 
-<div class="container-fluid">
-        
-   
+<div class="well">
     <div class="row">
-        <div class="jumbotron">
-            <form class="text-center">
-                <button style="width: 50%;font-size:2em;" class="btn-lg withripple btn btn-raised btn-info" formaction="<?php echo base_url() . 'index.php/resident/categories' ?>"><br>Start a new test!<br><br></button>
+        <div class="col-sm-6 col-sm-offset-3">
+            <form>
+                <button data-toggle="popover" title="" data-container="body" style="width:100%;font-size:2.5vmax;"
+                        class="popup btn-lg withripple btn btn-raised btn-info"
+                        data-content="<?= lang('resident_button_test_help') ?>" 
+                        formaction="<?= base_url() . 'index.php/resident/categories' ?>">
+                            <?= lang('resident_button_test') ?>
+                </button>
             </form>
-                    
-            <canvas class="center-block img-responsive" id="puzzle" style="height: 35vw;width:content-box"></canvas>
-            <script> loadPuzzle( "<?php echo base_url()?>", <?php echo json_encode($nrCompleted); ?> ); </script>
-            
         </div>
+    </div>
+    
+    <script> loadPuzzle("<?= base_url() ?>");</script>
+     <canvas class="center-block img-responsive" id="puzzle" style="height: 35vw;width:content-box"></canvas>
+            <script> loadPuzzle( "<?php echo base_url()?>", <?php echo json_encode($nrCompleted); ?> ); </script>
+</div>
 
 <?php if ($display_login_notification == true) { ?>
-	<script type="text/javascript">
-		$(document).ready(function () {
-			$.snackbar({content: '<?= lang( 'resident_welcome_snackbar' ) ?>' });
-		});
-	</script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $.snackbar({content: '<?= lang('resident_welcome_snackbar') ?>'});
+        });
+    </script>
 <?php } ?>
