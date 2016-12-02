6c528d68a1a1f33731e193345422f637558f8809
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 02:24:05 +0000

    Added language support on resident/completed.

--- application/views/resident/resident_completed.php
+++ application/views/resident/resident_completed.php
@@ -1,17 +1,14 @@
 <div class="well container-fluid">
-    <div class=" container row">
-        <p>
-                Congratulations, you've just completed the category {category}.
-        </p>
-        <p>
-                <i>Show animation in which they receive a puzzle piece.</i>
-        </p>
-    </div>
-    <div class="row">
-        <div class="col-xs-3">
-            <form action=<?php echo base_url().'index.php/resident/categories' ?> method="POST">
-                <input class="btn btn-raised btn-default" type="submit" name="Categories" value="Do a new category!" style="size: 100%">
-            </form>
-        </div>
-    </div>
+	<div class=" container row">
+		<p>
+			<?= lang( 'resident_completed_explanation' ) ?>
+		</p>
+	</div>
+	<div class="row">
+		<div class="col-xs-3">
+			<form action=<?php echo base_url().'index.php/resident/categories' ?> method="POST">
+				<input class="btn btn-raised btn-default" type="submit" name="Categories" value="<?= lang( 'resident_completed_start_new' ) ?>" style="size: 100%">
+			</form>
+		</div>
+	</div>
 </div>
