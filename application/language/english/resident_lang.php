a52b229a71e257ed22693cb1a2d7457ed3d7025b
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 08:31:25 +0000

    Fixed some language typos

--- application/language/english/resident_lang.php
+++ application/language/english/resident_lang.php
@@ -14,14 +14,14 @@ $lang[ 'resident_welcome_snackbar' ] = 'Hello {name}, you succesfully logged in.
 $lang[ 'resident_button_test' ] = 'Start a new test!';
 $lang[ 'resident_button_test_help' ] = 'Click on \'Start a new test!\' to start giving feedback.';
 
-$lang[ 'resident_puzzle_help' ] = '<-- You can see your progress on the puzzle here.';
+$lang[ 'resident_puzzle_help' ] = 'You can see your progress on the puzzle here.';
 
 // gallery
-$lang[ 'resident_gallery_explanation' ] = 'Your finished puzzles will be displaeyd here.';
+$lang[ 'resident_gallery_explanation' ] = 'Your finished puzzles will be displayed here.';
 
 // categories
 $lang[ 'resident_categories_explanation' ] = 'Select a category';
-$lang[ 'resident_categories_help' ] = 'Choose one of the three visible categories to continue.';
+$lang[ 'resident_categories_help' ] = 'Choose one of the visible categories to continue.';
 
 // question
 $lang[ 'resident_question_back' ] = 'Go Back';
