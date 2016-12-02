570394d8c7e05bf6115c489bdfcebd637cfd266a
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 01:59:45 +0000

    Implemented language support on resident_home.

--- trunk/application/views/resident/resident_home.php
+++ trunk/application/views/resident/resident_home.php
@@ -1,31 +1,33 @@
-
 <script>
-    $(document).ready(function () {
-            $('#popover').popover({
-            content: 'Popover content',
-            trigger: 'manual'
-        });
-        });
-
+	$(document).ready(function () {
+		$('#popover').popover({
+			content: 'Popover content',
+			trigger: 'manual'
+		});
+	});
 </script>
-    
-        <div class="well">
-            <div class="row">
-                <div class="col-sm-6 col-sm-offset-3">
-                     <form>
-                         <button data-toggle="popover" title="" data-container="body" data-content="Click on 'Start a new test!' to start giving feedback!" style="width:100%;font-size:2.5vmax;" class="popup btn-lg withripple btn btn-raised btn-info" formaction="<?php echo base_url() . 'index.php/resident/categories' ?>">Start a new test!</button>
-                    </form>
-                </div>
-            </div>
-            <canvas data-toggle="popover" title="" data-container="body" data-content="<- You can see your progress on the puzzle here" class="center-block img-responsive" id="puzzle" style="height: 35vw;width:auto"></canvas>
-            <script> loadPuzzle("<?php echo base_url() ?>");</script>
 
-        </div>
+<div class="well">
+	<div class="row">
+		<div class="col-sm-6 col-sm-offset-3">
+			<form>
+				<button data-toggle="popover" title="" data-container="body" style="width:100%;font-size:2.5vmax;"
+						class="popup btn-lg withripple btn btn-raised btn-info"
+						data-content="<?= lang( 'resident_button_test_help' ) ?>" 
+						formaction="<?= base_url() . 'index.php/resident/categories' ?>">
+								<?= lang( 'resident_button_test' ) ?>
+				</button>
+			</form>
+		</div>
+	</div>
+	<canvas data-toggle="popover" title="" data-container="body" data-content="<?= lang( 'resident_puzzle_help' ) ?>" class="center-block img-responsive" id="puzzle" style="height: 35vw;width:auto"></canvas>
+	<script> loadPuzzle("<?= base_url() ?>");</script>
+</div>
 
 <?php if ($display_login_notification == true) { ?>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $.snackbar({content: 'Hello {name}, you succesfully logged in!'});
-        });
-    </script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$.snackbar({content: '<?= lang( 'resident_welcome_snackbar' ) ?>' });
+		});
+	</script>
 <?php } ?>
