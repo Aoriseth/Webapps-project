87b264c649374755065eb4048e4ccf779a0aa2f8
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 02:51:43 +0000

    Improved consistency between languages.

--- trunk/application/language/english/resident_lang.php
+++ trunk/application/language/english/resident_lang.php
@@ -9,10 +9,10 @@ $lang[ 'resident_navbar_gallery' ] = 'Gallery';
 $lang[ 'resident_navbar_gallery_help' ] = 'Click on \'gallery\' to view previous puzzles.';
 
 // home
-$lang[ 'resident_welcome_snackbar' ] = 'Hello {name}, you succesfully logged in!';
+$lang[ 'resident_welcome_snackbar' ] = 'Hello {name}, you succesfully logged in.';
 
 $lang[ 'resident_button_test' ] = 'Start a new test!';
-$lang[ 'resident_button_test_help' ] = 'Click on \'Start a new test!\' to start giving feedback!';
+$lang[ 'resident_button_test_help' ] = 'Click on \'Start a new test!\' to start giving feedback.';
 
 $lang[ 'resident_puzzle_help' ] = '<-- You can see your progress on the puzzle here.';
 
@@ -21,11 +21,11 @@ $lang[ 'resident_gallery_explanation' ] = 'Your finished puzzles will be displae
 
 // categories
 $lang[ 'resident_categories_explanation' ] = 'Select a category';
-$lang[ 'resident_categories_help' ] = 'Choose one of the three visible categories to continue';
+$lang[ 'resident_categories_help' ] = 'Choose one of the three visible categories to continue.';
 
 // question
 $lang[ 'resident_question_back' ] = 'Go Back';
 
 // completed
-$lang[ 'resident_completed_explanation' ] = 'Congratulations, you\'ve just completed the category {category}.';
-$lang[ 'resident_completed_start_new' ] = 'Do a new category!';
+$lang[ 'resident_completed_explanation' ] = 'Congratulations, you\'ve just completed the category <i>{category}</i>.';
+$lang[ 'resident_completed_start_new' ] = 'Choose a new category!';
