74529c6ec14b085323dc3412b5e59b516b37280e
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 08:25:22 +0000

    Simplified and cleaned up resident main page

--- trunk/application/views/resident/resident_main.php
+++ trunk/application/views/resident/resident_main.php
@@ -3,46 +3,47 @@
     <head>
         <meta charset="utf-8">
         <title>Grace Age: Resident</title>
-       {include}
-
-        <!-- Load scripts -->
-        
+        <!--Include common scripts and css-->
+        {include}
+        <!-- Load  Unique scripts -->
         <script src="<?php echo base_url(); ?>assets/js/puzzle.js"></script>
-
     </head>
-    
+
     <body>
+        <!--Navigation bar of this page-->
         {navbar}
+        
+        <!-- the main element of the page (puzzle, etc) -->
         <div class="container" id="container_resident">
-            
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
-                    <!--div class="jumbotron"-->       
                     <div>
-                    {content}	
-                    </div> <!-- the main element of the page (login form etc) -->
-                    <!--/div-->
+                        
+                        {content}	
+                    </div> 
                 </div>
             </div>
-            
-            
         </div>
-        <script>
-        $(document).ready(function () {
-          $('#trigger').click(function(event){
-            $('.popup').popover('toggle');
-            event.stopPropagation();
-          });
-          
-          $(window).click(function() {
-            //Hide the menus if visible
-            $('.popup').popover('hide');
-            });
-        });
-        </script>
-		<button id="trigger" class="withripple btn btn-warning btn-raised" style="height:3em;width:7em;position:fixed;bottom:30px;right:30px;">
-			<?= lang( 'resident_button_help' ) ?>
-		</button>
+
+        <!--Help button for resident pages-->
+        <button id="trigger" class="btn btn-warning btn-raised" style="height:3em;width:7em;position:fixed;bottom:30px;right:30px;">
+            <?= lang('resident_button_help') ?>
+        </button>
     </body>
 </html>
+
+<!-- Javascripting-->
+<script>
+    $(document).ready(function () {
+        $('#trigger').click(function (event) {
+            $('.popup').popover('toggle');
+            event.stopPropagation();
+        });
+
+        $(window).click(function () {
+            //Hide the menus if visible
+            $('.popup').popover('hide');
+        });
+    });
+</script>
 <script>$.material.init();</script>
