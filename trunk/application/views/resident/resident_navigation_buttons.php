ab8fcb505467ee2490899ed46e872060942a1c40
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 09:40:03 +0000

    removed unused navigation buttons view

--- trunk/application/views/resident/resident_navigation_buttons.php
+++
@@ -1,8 +0,0 @@
-<!-- I am currently on page '{page}' -->
-<form>
-    <button style="width: 100%" <?php if ( $page == 'home' ) { ?>disabled<?php } ?> class=" btn-lg withripple btn btn-raised btn-default" formaction="<?php echo base_url() . 'index.php/resident/home' ?>">Home</button>
-</form>
-
-<form>
-    <button style="width: 100%" class=" btn-lg withripple btn btn-raised btn-warning" formaction="<?php echo base_url() . 'index.php/logout' ?>">Logout</button>
-</form>
