9c89da1a1f71821fffc451c3879c435a1f735b54
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-02 08:20:45 +0000

    Removed some unnecessary stuff

--- trunk/application/views/login/login_main.php
+++ trunk/application/views/login/login_main.php
@@ -18,26 +18,6 @@
                 </div>
             </div>
         </div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        
-      </div>
-      <div class="modal-body">
-          
-          <div style='position:relative;padding-bottom:calc(100% / 1.78)'><iframe src='https://gfycat.com/ifr/PerfectUnsungCub' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div>
-      </div>
-    </div>
-  </div>
-</div>
-        
-        
-        
     </body>
 </html>
 <script>$.material.init();</script>
