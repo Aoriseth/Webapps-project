afd5e3c833a8f1bed4ee2dd2364d791d33335ecd
r0372268 <r0372268@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-03 11:03:58 +0000

    fix progress bar

--- application/views/resident/resident_question.php
+++ application/views/resident/resident_question.php
@@ -51,8 +51,8 @@
 
             <div class="col-sm-10" id="progress_col">
                 <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="progresBar"
-                         style="width: <?php echo ( (int) $index / (int) $category_size * 100 ) ?>%">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="progressBar"
+                         >
                     </div>
                 </div>
             </div>
