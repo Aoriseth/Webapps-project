8b84baa02024cb16d1d8d0f1e33602d965a515bb
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-08 19:23:30 +0000

    added help to gallery page

--- trunk/application/views/resident/resident_gallery.php
+++ trunk/application/views/resident/resident_gallery.php
@@ -1,4 +1,4 @@
-<div class="panel container-fluid">
+<div data-placement="bottom" data-toggle="popover" title="" data-container="body" data-content="<?= lang( 'r_gallery_help' ) ?>" class="popup panel container-fluid">
     <br>
     <p class="txScale">
         <?= lang('r_gallery_explanation') ?>
