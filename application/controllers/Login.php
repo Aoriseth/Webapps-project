2db02a127c867c752e552948e48a97e16fa752ac
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-08 02:05:15 +0000

    Refactoring controller login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,10 +7,12 @@ class Login extends CI_Controller {
 		parent::__construct();
 
 		// redirect to base if the user is already logged in
-		if ( $this->session->is_logged_in ) { redirect( base_url() ); }
+		if ( $this->session->is_logged_in ) {
+			redirect( base_url() );
+		}
 
 		// load appropriate language file
-		if ( ! isset( $this->session->language ) ) {
+		if ( !isset( $this->session->language ) ) {
 			// fallback on default
 			$this->session->language = $this->config->item( 'language' );
 		}
@@ -18,8 +20,8 @@ class Login extends CI_Controller {
 		$this->lang->load( 'login', $this->session->language );
 
 		// models
-		$this->load->model( 'Login_model' );
 		$this->load->model( 'FaceRecKeys_model' );
+		$this->load->model( 'Login_model' );
 	}
 
 	public function index()
@@ -33,13 +35,13 @@ class Login extends CI_Controller {
 		$this->parser->parse( 'login/login_main', $data );
 	}
 
-	public function en()	// temporary test function
+	public function en()
 	{
 		$this->session->language = 'english';
 		redirect( base_url() );
 	}
 
-	public function nl()	// temporary test function
+	public function nl()
 	{
 		$this->session->language = 'nederlands';
 		redirect( base_url() );
@@ -48,46 +50,46 @@ class Login extends CI_Controller {
 	public function get_facial_recognition_tokens()
 	{
 		// only allow AJAX requests
-		if ( ! $this->input->is_ajax_request() ) {
-			redirect('404');
+		if ( !$this->input->is_ajax_request() ) {
+			redirect( '404' );
 		}
 
 		$result = $this->FaceRecKeys_model->getKeys();
 
-	    header( 'Content-Type: application/json' );
+		header( 'Content-Type: application/json' );
 		echo json_encode( $result );
 	}
 
 	public function ajax()
 	{
 		// only allow AJAX requests
-		if ( ! $this->input->is_ajax_request() ) {
-			redirect('404');
+		if ( !$this->input->is_ajax_request() ) {
+			redirect( '404' );
 		}
 
 		// check if POST is set correct (at least kind of)
-		if ( ! isset( $_POST[ 'username' ] ) || ! isset( $_POST[ 'password' ] ) ) {
-		    header( 'Content-Type: application/json' );
+		if ( !isset( $_POST[ 'username' ] ) || !isset( $_POST[ 'password' ] ) ) {
+			header( 'Content-Type: application/json' );
 			echo json_encode( array( 'success' => false, 'error' => 'Username or password field not set.' ) );
 			return;
 		}
 
 		$username = $this->input->post( 'username' );
 		$password = $this->input->post( 'password' );
-                
+
 		$result = $this->Login_model->login( $username, $password );
 
 		if ( $result[ 'succeeded' ] == true ) {
 			$this->setup_login( $result[ 'person' ] );
 
-		    header( 'Content-Type: application/json' );
+			header( 'Content-Type: application/json' );
 			echo json_encode( array( "success" => true ) );
 		} else {
-		    header( 'Content-Type: application/json' );
+			header( 'Content-Type: application/json' );
 			echo json_encode( array( 'success' => false, 'error' => $result[ 'error' ] ) );
 		}
 	}
-	
+
 	private function setup_login( $person )
 	{
 		$this->session->is_logged_in = true;
