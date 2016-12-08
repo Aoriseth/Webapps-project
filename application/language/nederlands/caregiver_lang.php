91f39784bebe9da8c5cb1ddf7bc93e27ccc9360c
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-08 02:39:57 +0000

    Refactoring caregiver navbar language support to differentiate naming

--- application/language/nederlands/caregiver_lang.php
+++ application/language/nederlands/caregiver_lang.php
@@ -4,10 +4,10 @@
 $lang[ 'c_title' ] = lang( 'common_app' ).': Verzorger';
 
 // navbar
-$lang[ 'c_home' ] = 'Thuis';
-$lang[ 'c_overview' ] = 'Overzicht residenten';
-$lang[ 'c_groups' ] = 'Groep selectie';
-$lang[ 'c_statistics' ] = 'Statistieken';
+$lang[ 'c_navbar_home' ] = 'Thuis';
+$lang[ 'c_navbar_overview' ] = 'Overzicht residenten';
+$lang[ 'c_navbar_groups' ] = 'Groep selectie';
+$lang[ 'c_navbar_statistics' ] = 'Statistieken';
 
 // home
 $lang[ 'c_home_body' ] = 'Hallo {name}.';
