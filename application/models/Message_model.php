a37b5a49b5c5224104f9e16464d5fb0d251d8afe
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-08 21:23:59 +0000

    Added functions in Picture_model for deleting pictures from server and database.

--- application/models/Message_model.php
+++ application/models/Message_model.php
@@ -1,8 +1,28 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Message_model extends CI_Model {
 
+    public function __construct() {
+        parent::__construct();
+    }
+	
+	/**
+	 * Return all unread messages posted by the given resident.
+	 */
+	function getMessagesFrom( $residentID ) {
+		
+	}
+	
+	/**
+	 * Return the n last posted unread messages.
+	 */
+	function getNLastMessages( $n ) {
+		$language = 'English';
+		$query = $this->db->query(
+			"SELECT resident_id, first_name, last_name, message, posted_on"
+			. " FROM a16_webapps_3.message_view"
+        );
+        return $query->result();
+	}
+	
+}
