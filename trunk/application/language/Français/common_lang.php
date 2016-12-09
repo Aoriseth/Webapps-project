f0ece9f3bb95a3bcaeeb05c55e4416c850a84435
r0652164 <r0652164@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-09 00:50:19 +0000

    adding french language file

---
+++ trunk/application/language/Français/common_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang[ 'common_app' ] = 'Grace Age';
+
+$lang[ 'common_login' ] = 'Connection';
+$lang[ 'common_logout' ] = 'Deconnection ';
+
+$lang[ 'common_welcome_snackbar' ] = 'Bonjour {name}, vous êtes connecté(e) avec succès .';
