784f1a225364b633c9200d4b468584b151590d60
r0368818 <r0368818@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-11 15:14:57 +0000

    made a chart to display the course of a category from a resident, how the score evolved

--- application/views/caregiver/caregiver_resident.php
+++ application/views/caregiver/caregiver_resident.php
@@ -13,20 +13,36 @@
     </div>
     <div class="row">
         <div class="col-xs-6">
-            eventueel problematische topics (-50?)
+            Problematic categories: </br>
+            <?php foreach ($problematicCategories as $category){?>
+                <?php echo $category;?>
+            </br>
+            
+            <?php }?>
         </div>
         <div class="col-xs-6">
             grafiek die verloop toont van bepaalde categorie over over alle sessies
+            <form class="form-group" method="POST" id="chart2_form" name="chart2Form">
+                <select class="form-control" name="categories" id="categories_select" onchange="chart2function()" onload="chart2function()">
+                        <?php foreach ($categories as $category) { ?>   
+                                <option value=<?= json_encode($category->category_set); ?> ><?php echo $category->category; ?></option>
+                        <?php } ?>           
+                </select>
+
+
+            </form>
+            <div id="chart2_div" style="width:100%"></div>
         </div>
     </div>
 </div>
 
 <script type="text/javascript">
 	
-
+        var formChart2 = document.getElementById('chart2_form');
 	google.charts.load('current', {'packages': ['corechart']});
 	
         chart1function();
+        chart2function();
 	//formChart1.addEventListener('submit', function(e) {
 	function chart1function() {
 		console.log('chart 1 submitted');
@@ -58,9 +74,50 @@
 				function drawChart() {
 
 					var data = new google.visualization.DataTable();
-					dataChart(Yaxis, Xaxis, data, "chart1_div", '<?= lang( 'c_statistics_all_category_individual' ) ?>');
+					columnChart(Yaxis, Xaxis, data, "chart1_div", '<?= lang( 'c_statistics_all_category_individual' ) ?>');
 				}
 
+
+			}
+		});
+		return false;
+	}
+        
+        function chart2function() {
+		console.log('chart 2 submitted');
+		//e.preventDefault();
+		var selects = formChart2.getElementsByTagName('select');
+
+
+		var category = parseInt(selects[0].value);
+
+
+		$.ajax({
+			type: "POST",
+			url: "<?php echo base_url() ?>index.php/caregiver/load_category_course_chart",
+			data: {category: category, resident: "{id}"},
+			dataType: "text",
+			cache: false,
+
+			success: function (data) {
+				var Yaxis = [];
+				var Xaxis = [];
+				console.log(data);
+				var response = JSON.parse(data);
+				console.log(response);
+
+				Xaxis = response[0];
+				Yaxis = response[1];
+
+
+				google.charts.setOnLoadCallback(drawChart2);
+
+
+				function drawChart2() {
+
+					var data = new google.visualization.DataTable();
+					lineChart(Yaxis, Xaxis, data, "chart2_div", 'verloop van categorie');
+				}
 
 			}
 		});
--- assets/js/statistics/chartCallbacks.js
+++ assets/js/statistics/chartCallbacks.js
@@ -1,6 +1,6 @@
 
 
-      function dataChart(yAxis, xAxis, data, chartId, chartTitle) {
+      function columnChart(yAxis, xAxis, data, chartId, chartTitle) {
          
         // Create the data table.
         //var data = new google.visualization.DataTable();
@@ -29,6 +29,41 @@
 
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.ColumnChart(document.getElementById(chartId));
+        chart.draw(data, options);
+        
+        
+    
+      }
+      
+      function lineChart(yAxis, xAxis, data, chartId, chartTitle) {
+         
+        // Create the data table.
+        //var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Category');
+        data.addColumn('number', 'Score');
+        var rows = [];
+
+
+        for (var index = 0; index < yAxis.length; index++) {
+            rows.push([yAxis[index], xAxis[index]]);
+
+        }
+            
+        data.addRows(rows);
+        // Set chart options
+        var options = {'title':chartTitle,
+                       'width':400,
+                       'height':400,
+                        vAxis: {gridlines: { count: 5 },
+                                minValue: 0,
+                                maxValue: 100
+                                }
+
+                        
+                    };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.LineChart(document.getElementById(chartId));
         chart.draw(data, options);
         
         
