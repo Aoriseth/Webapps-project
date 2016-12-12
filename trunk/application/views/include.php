3ba6ce35c5e2033bd3058b9eb8c56c1af44f4932
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-12 23:43:04 +0000

    Removed facial recognition stuff: login button, javascript functions

--- trunk/application/views/include.php
+++ trunk/application/views/include.php
@@ -1,23 +1,21 @@
 <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <!-- Load CSS -->
-                
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
-                <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-                
-                <!-- Load scripts -->
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/material.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/faceRecognition/FRLambdaAPI.js"></script>
-                <script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
-                    
-                <!-- Activate Material Design , do this manually on the bottom of the main page-->
-                <!--<script>$.material.init();</script>-->
-                 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Load CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+<!-- Load scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/material.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
+
+<!-- Activate Material Design , do this manually on the bottom of the main page-->
+<!--<script>$.material.init();</script>-->
 		
--- trunk/application/views/login/login_camera.php
+++ trunk/application/views/login/login_camera.php
@@ -1,6 +1,7 @@
 <script src="<?= base_url();?>assets/js/QR-code/qcode-decoder.min.js" type="text/javascript"></script>
 <script src="<?= base_url();?>assets/js/QR-code/QRcodeRecog.js" type="text/javascript"></script>
 <script> setBaseURL("<?= base_url();?>");</script>
+
 <p class="txScale">
 	<?= lang( 'login_camera_explanation' ) ?>
 </p>
@@ -9,13 +10,6 @@
 	<?= lang( 'login_camera_scrolldown' ) ?>
 </p>
 
-<video id="camfr" width="90%" height="auto" autoplay="true">
-
-</video>
-<!--<script src="<?php echo base_url(); ?>assets/js/faceRecognition/capture.js" type="text/javascript"></script>-->
-<canvas id = "canvasVideo" hidden="true" width="90%" height="auto" ></canvas>
-<img id="photoFR" width="90%" height="auto" > 
-
-
-<button class="btn-lg btn btn-default btn-raised" id="facialLoginButton"><?= lang( 'common_login' ) ?></button>
-            
+<video id="camfr" width="90%" height="auto" autoplay="true" transform="scaleX(-1)" />
+<canvas id = "canvasVideo" hidden="true" width="90%" height="auto" />
+<img id="photoFR" width="90%" height="auto" />
