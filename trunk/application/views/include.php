9f4c8f1b31de407b7cef6d9ea732a7e4e9dffaf2
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-13 00:11:59 +0000

    Added favicon

--- trunk/application/views/include.php
+++ trunk/application/views/include.php
@@ -1,21 +1,23 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!-- Favicon -->
+<link rel="icon" href="<?=base_url()?>/assets/imgs/favicon.ico" type="image/gif">
+
 <!-- Load CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-material-design.css">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/ripples.css">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/snackbar.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/bootstrap-material-design.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/ripples.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/main.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/snackbar.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
 <!-- Load scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/material.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/ripples.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/snackbar.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/material.js"></script>
+<script src="<?= base_url(); ?>assets/js/ripples.js"></script>
+<script src="<?= base_url(); ?>assets/js/snackbar.min.js"></script>
 
 <!-- Activate Material Design , do this manually on the bottom of the main page-->
 <!--<script>$.material.init();</script>-->
-		
