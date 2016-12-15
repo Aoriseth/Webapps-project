c780a3895daff621bb0847b4717c4c782f0abdc5
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-15 21:57:36 +0000

    layered background for login page

--- trunk/application/views/login/login_navbar.php
+++ trunk/application/views/login/login_navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-fixed-top navbar-inverse">
+<nav class="navbar navbar-fixed-top navbar-inverse" style="background-color: #512DA8">
 	<div class="container">
 		<div class="navbar-header">
 			<a class="navbar-brand"><?= lang( 'common_app' ) ?></a>
@@ -13,3 +13,6 @@
 		</div>
 	</div>
 </nav>
+<div style="top:0px;height:250px;width:100%;background-color: #673ab7;position:absolute;">
+    
+</div>
