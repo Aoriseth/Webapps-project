8110d9418507b2fada91619825631bc1711dc4cb
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-15 20:41:14 +0000

    Added page specific panel and styling

--- application/views/caregiver/caregiver_home.php
+++ application/views/caregiver/caregiver_home.php
@@ -1,16 +1,20 @@
-<p class="txScale"><?= lang( 'c_home_body' ) ?></p>
+<div class="panel container-fluid">
+    <br>
+    <p class="tlScale"><?= lang('c_home_body') ?></p>
 
-<?php if ($display_login_notification == true) { ?>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $.snackbar({content: '<?= lang( 'common_welcome_snackbar' ) ?>'});
-        });
-    </script>
-<?php } ?>
+    <?php if ($display_login_notification == true) { ?>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $.snackbar({content: '<?= lang('common_welcome_snackbar') ?>'});
+            });
+        </script>
+    <?php } ?>
 
-    
-problematische residenten 
 
-problematische topics + bar chart alle categorieen
+        <p class="txScale">problematische residenten <p>
+
+        <p class="txScale">problematische topics + bar chart alle categorieen</p>
+    <br>
+</div>
 
 
