d8630721651bcf3be889b406ceade9aa18669bec
s0188508 <s0188508@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-15 01:18:27 +0000

    Added functions/queries for adding/getting session dates/scores to/from the database.

--- application/models/Score_model.php
+++ application/models/Score_model.php
@@ -16,7 +16,7 @@ class Score_model extends CI_Model {
 	 */
 	
 	/**
-	 * 
+	 * Add the total score of this session to the database.
 	 */
 	function addSessionScore( $residentID ) {
 		$currentSession = ( $this->Resident_model->getSessionsCompleted( $residentID ) ) + 1;
@@ -29,13 +29,118 @@ class Score_model extends CI_Model {
 		$this->db->insert( 'a16_webapps_3.session_scores', $array );
 	}
 	
-	function getAllCompletedSessionDates() {
-		
+	/**
+	 * Get the completed sessions of the given resident and the date on which
+	 * these sessions were completed.
+	 */
+	function getAllCompletedSessionDates( $residentID ) {
+		$query = $this->db->query(
+			"SELECT session, completed_on "
+			. "FROM a16_webapps_3.session_scores "
+			. "WHERE resident_id='$residentID' "
+			. "ORDER BY session ASC"
+		);
+		return $query->result();
 	}
 	
-	function getAllCompletedSessionScores() {
-		
+	/**
+	 * Get the completed sessions of the given resident and the score of each
+	 * completed session.
+	 */
+	function getAllCompletedSessionScores( $residentID ) {
+		$query = $this->db->query(
+			"SELECT session, score "
+			. "FROM a16_webapps_3.session_scores "
+			. "WHERE resident_id='$residentID' "
+			. "ORDER BY session ASC"
+		);
+		return $query->result();
 	}
+	
+	/**
+	 * Get the completed sessions of the given resident and the score of each
+	 * completed session.
+	 * 
+	 * Use this function only if you need both the scores and the dates
+	 * of the completed sessions.
+	 */
+	function getAllCompletedSessionScoresAndDates( $residentID ) {
+		$query = $this->db->query(
+			"SELECT session, score, completed_on "
+			. "FROM a16_webapps_3.session_scores "
+			. "WHERE resident_id='$residentID' "
+			. "ORDER BY session ASC"
+		);
+		return $query->result();
+	}
+	
+	/**
+	 * Get the most recently completed session and date
+	 * of all residents.
+	 */
+	function getLastCompletedSessionDatesOfAllResidents() {
+		$query = $this->db->query(
+			"SELECT session, completed_on "
+			. "FROM ( "
+				. "SELECT a16_webapps_3.session_scores.*, row_number() OVER "
+				. "(PARTITION BY resident_id ORDER BY completed_on DESC) AS rn "
+				. "FROM a16_webapps_3.session_scores "
+				. ") nested_table "
+			. "WHERE nested_table.rn = 1"
+		);
+		return $query->result();
+	}
+	
+	/**
+	 * Get the most recently completed session and score
+	 * of all residents.
+	 */
+	function getLastCompletedSessionScoresOfAllResidents() {
+		$query = $this->db->query(
+			"SELECT session, score "
+			. "FROM ( "
+				. "SELECT a16_webapps_3.session_scores.*, row_number() OVER "
+				. "(PARTITION BY resident_id ORDER BY completed_on DESC) AS rn "
+				. "FROM a16_webapps_3.session_scores "
+				. ") nested_table "
+			. "WHERE nested_table.rn = 1"
+		);
+		return $query->result();
+	}
+	
+	/**
+	 * Get the most recently completed session and score
+	 * of all residents.
+	 * 
+	 * Use this function only if you really need both the score of
+	 * the session and the date on which it was finished.
+	 */
+	function getLastCompletedSessionScoresAndDatesOfAllResidents() {
+		$query = $this->db->query(
+			"SELECT session, score, completed_on "
+			. "FROM ( "
+				. "SELECT a16_webapps_3.session_scores.*, row_number() OVER "
+				. "(PARTITION BY resident_id ORDER BY completed_on DESC) AS rn "
+				. "FROM a16_webapps_3.session_scores "
+				. ") nested_table "
+			. "WHERE nested_table.rn = 1"
+		);
+		return $query->result();
+	}
+	
+	/**
+	 * Compute the total score of the current session for the given resident.
+	 */
+	private function computeTotalScore( $residentID ) {
+		//TODO -> first category scores need to be done.
+		
+		//Plan:
+		//- get all category scores from the current session
+		//- use the category weights to get the score of the category
+		//- compute average of scores and rescale to 100
+		//- return the total score
+	}
+	
 	
 	/**
 	 * ----------------------------------------------
@@ -43,4 +148,11 @@ class Score_model extends CI_Model {
 	 * ----------------------------------------------
 	 */
 	
+	/**
+	 * Compute the score of the given category set answered by the resident
+	 * within the current session.
+	 */
+	private function computeCategoryScore( $residentID, $categorySet ) {
+		//TODO
+	}
 }
