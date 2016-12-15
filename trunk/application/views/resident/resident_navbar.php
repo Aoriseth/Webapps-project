83123e4756758c42ed055f037023b9f171912af8
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-15 21:58:47 +0000

    double layer for resident pages

--- trunk/application/views/resident/resident_navbar.php
+++ trunk/application/views/resident/resident_navbar.php
@@ -1,5 +1,5 @@
 <!-- I am currently on page '{page}' -->
-<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #512DA8">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
@@ -7,26 +7,29 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" ><?= lang( 'common_app' ) ?></a>
+            <a class="navbar-brand" ><?= lang('common_app') ?></a>
         </div>
         <div class="navbar-collapse collapse navbar-responsive-collapse">
             <ul class="nav navbar-nav">
-                <li <?php if ( $page == 'home' ) { ?>class="active"<?php } ?> ><a href="<?php echo base_url() . 'index.php/resident/home' ?>"><?= lang( 'r_navbar_home' ) ?></a></li>
-                <li data-placement="bottom" data-toggle="popover" title="" data-container="body" data-content="<?= lang( 'r_navbar_gallery_help' ) ?>" class="popup <?php if ( $page == 'gallery' ) { ?>active<?php } ?>" ><a href="<?php echo base_url() . 'index.php/resident/gallery' ?>"><?= lang( 'r_navbar_gallery' ) ?></a></li>
+                <li <?php if ($page == 'home') { ?>class="active"<?php } ?> ><a href="<?php echo base_url() . 'index.php/resident/home' ?>"><?= lang('r_navbar_home') ?></a></li>
+                <li data-placement="bottom" data-toggle="popover" title="" data-container="body" data-content="<?= lang('r_navbar_gallery_help') ?>" class="popup <?php if ($page == 'gallery') { ?>active<?php } ?>" ><a href="<?php echo base_url() . 'index.php/resident/gallery' ?>"><?= lang('r_navbar_gallery') ?></a></li>
             </ul>
-            
+
             <ul class="nav navbar-nav navbar-right">
-                <!--<li><a id="trigger" href="#"><?= lang( 'r_button_help' ) ?></a></li>-->
-                <li><a href="<?php echo base_url() . 'index.php/logout' ?>"><?= lang( 'common_logout' ) ?></a></li>
+                <!--<li><a id="trigger" href="#"><?= lang('r_button_help') ?></a></li>-->
+                <li><a href="<?php echo base_url() . 'index.php/logout' ?>"><?= lang('common_logout') ?></a></li>
                 <li><a></a></li>
                 <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
             </ul>
         </div>
-            
-                
 
-        </div>
+
+
     </div>
+</div>
+<div style="top:0px;height:250px;width:100%;background-color: #673ab7;position:absolute;">
+    
+</div>
 
 
 
