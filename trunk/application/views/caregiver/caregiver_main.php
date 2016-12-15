06d944f429de0353731d89af4a17fe63538ccfc2
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-15 20:44:02 +0000

    Use more of the available screen space (better for tablets)

--- trunk/application/views/caregiver/caregiver_main.php
+++ trunk/application/views/caregiver/caregiver_main.php
@@ -28,7 +28,7 @@
 
         <div class="container">
             <div class="row">
-                <div class="col-md-offset-1 col-md-10">
+                <div class="col-md-12">
                     <div class="container-fluid">
                         {content}
                     </div>
