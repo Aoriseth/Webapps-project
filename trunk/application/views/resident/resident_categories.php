4baf06ab25ea9e3c61b294fc08b00cb04abd6e2f
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-16 09:11:34 +0000

    made icons round to fit the buttons on the question page

--- trunk/application/views/resident/resident_categories.php
+++ trunk/application/views/resident/resident_categories.php
@@ -13,12 +13,12 @@
                     <div class="col-sm-4" >
                         <form style='text-align:center' action="<?php echo base_url() . 'index.php/resident/question' ?>" method="GET">
                             <!--input class="btn btn-default" type="submit" name="category" value="<?php echo $category->category ?>" class="text-center"-->
-                            <button type="submit" name="category" value="<?php echo $category->category ?>" class="btn btn-raised btn-default text-center">
+                            <button style="width:13vw;height:13vw;" type="submit" name="category" value="<?php echo $category->category ?>" class="btn btn-fab btn-default text-center">
                                 <?php if ($category->category === "Safety and security") { ?>
-                                <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/295/295666.png">
+                                <img style="width:9vw;" src="http://image.flaticon.com/icons/png/512/295/295666.png">
                                     <!--<span style='font-size:10vw' class="glyphicon glyphicon-facetime-video"></span>-->
                                 <?php } elseif ($category->category === "Food and meals") { ?>
-                                <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/272/272155.png">
+                                <img style="width:8vw;" src="http://image.flaticon.com/icons/png/512/272/272155.png">
                                     <!--<span style='font-size:10vw' class="glyphicon glyphicon-cutlery"></span>--> 
                                 <?php } elseif ($category->category === "Comfort") { ?>
                                 <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/148/148102.png">
@@ -27,16 +27,16 @@
                                     <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/265/265667.png">
                                     <!--<span style='font-size:10vw' class="glyphicon glyphicon-heart"></span>--> 
                                 <?php } elseif ($category->category === "Activities") { ?>
-                                    <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/147/147206.png">
+                                    <img style="width:8vw;" src="http://image.flaticon.com/icons/png/512/147/147206.png">
                                     <!--<span style='font-size:10vw' class="glyphicon glyphicon-blackboard"></span>--> 
                                 <?php } elseif ($category->category === "Privacy") { ?>
-                                    <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/147/147040.png">
+                                    <img style="width:8vw;" src="http://image.flaticon.com/icons/png/512/147/147040.png">
                                     <!--<span style='font-size:10vw' class="glyphicon glyphicon-home"></span>--> 
                                 <?php } else { ?>
                                     <img style="width:10vw;" src="http://image.flaticon.com/icons/png/512/190/190177.png">
                                     <!--<span style='font-size:10vw' class="glyphicon glyphicon-edit"></span>-->
                                 <?php } ?>
-                            </button></br>
+                            </button><br><br>
                             <p style="font-size:2vmax"><?php echo $category->category ?></p>
 
 
