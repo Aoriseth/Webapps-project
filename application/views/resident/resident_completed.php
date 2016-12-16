40ad94a388c02be490acc0a4987fb5bcd74fa243
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-16 10:42:39 +0000

    updated styling made the tip look like a quote changed buttons styling

--- application/views/resident/resident_completed.php
+++ application/views/resident/resident_completed.php
@@ -1,17 +1,23 @@
 <div class="container-fluid panel ">
     <div class="container-fluid row">
         <br>
-        <p class="txScale">
+        <p class="tlScale">
             <?= lang('r_completed_explanation') ?>
-        </p><br>
+        </p><hr>
     </div>
-	<div class="tip">
-		{tip}
-	</div>
+	<div class="tlScale tip">
+            <blockquote>
+                <i>
+                    {tip}
+                </i>
+            </blockquote>
+		
+               
+	</div> <br>
     <div class="row">
         <div class="col-xs-3">
             <form action=<?php echo base_url() . 'index.php/resident/categories' ?> method="POST">
-                <input class="btn btn-raised btn-default" type="submit" name="Categories" value="<?= lang('r_completed_start_new') ?>" style="size: 100%">
+                <input class="btn btn-lg btn-default" type="submit" name="Categories" value="<?= lang('r_completed_start_new') ?>" style="size: 100%;color:#673AB7">
             </form>
             <br>
         </div>
