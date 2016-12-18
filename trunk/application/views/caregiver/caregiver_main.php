20e215ae8a1e3fa831a746a7268ff0fee47d23ba
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 19:53:31 +0000

    Removed unnecessary h3 in caregiver/overview + removed <head> section from caregiver_overview.php

--- trunk/application/views/caregiver/caregiver_main.php
+++ trunk/application/views/caregiver/caregiver_main.php
@@ -13,8 +13,12 @@
         <script src="<?php echo base_url(); ?>assets/js/statistics/chartCallbacks.js"></script>
         
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker3.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/datatables-colvis/1.1.2/css/dataTables.colVis.css"/>
+
         {include}
-        
 
         <!--	HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--	 WARNING: Respond.js doesn't work if you view the page via file:// -->
