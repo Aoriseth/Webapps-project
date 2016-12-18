93262e8d17e7e90843023f92697e5b21f6e82050
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 18:37:07 +0000

    added extra spacing to login form

--- trunk/application/views/login/login_manual.php
+++ trunk/application/views/login/login_manual.php
@@ -1,33 +1,33 @@
 <script src="<?= base_url(); ?>assets/js/login.js" type="text/javascript"></script>
 <script>
-$( function() {
-	$( "#login_form" ).submit( function( event ) {
-		login( '<?= base_url() ?>',null,null );
-		return false;
-	});
-});
+    $(function () {
+        $("#login_form").submit(function (event) {
+            login('<?= base_url() ?>', null, null);
+            return false;
+        });
+    });
 </script>
 
 <div>
     <p class="txScale">
-		<?= lang( 'login_manual_explanation' )?>
-	</p>
+        <?= lang('login_manual_explanation') ?>
+    </p>
 
-	<p span style="color:red" id="error_message">
-	</p>
+    <p span style="color:red" id="error_message">
+    </p>
 
-	<form method="POST" id="login_form">
-		<div class="container-fluid">
-			<div class="form-group label-floating">
-				<label class="control-label" for="focusedInput1"><?= lang( 'login_field_username' )?></label>
-				<input class="form-control" name="username" id="focusedInput1" required type="text">
-			</div>
-			<div class="form-group label-floating">
-				<label class="control-label" for="focusedInput2"><?= lang( 'login_field_password' )?></label>
-				<input class="form-control" name="password" id="focusedInput2" required type="password">
-			</div>
-		
-                    <input class="btn btn-lg btn-raised btn-info" style="background-color: #673AB7;" type="submit" name="login" value="<?= lang( 'common_login' ) ?>">
-		</div>
-	</form>
+    <form method="POST" id="login_form">
+        <div class="container-fluid">
+            <div class="form-group label-floating">
+                <label class="control-label" for="focusedInput1"><?= lang('login_field_username') ?></label>
+                <input class="form-control" name="username" id="focusedInput1" required type="text">
+            </div>
+            <div class="form-group label-floating">
+                <label class="control-label" for="focusedInput2"><?= lang('login_field_password') ?></label>
+                <input class="form-control" name="password" id="focusedInput2" required type="password">
+            </div>
+            <br>
+            <input class="btn btn-lg btn-raised btn-info" style="background-color: #673AB7;" type="submit" name="login" value="<?= lang('common_login') ?>">
+        </div>
+    </form>
 </div>
