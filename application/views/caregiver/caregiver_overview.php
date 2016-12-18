a994c230961f5a590f93e762564b3187e01038c8
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 19:53:31 +0000

    Removed unnecessary h3 in caregiver/overview + removed <head> section from caregiver_overview.php

--- application/views/caregiver/caregiver_overview.php
+++ application/views/caregiver/caregiver_overview.php
@@ -1,31 +1,14 @@
-<head> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker3.min.css"/>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/datatables-colvis/1.1.2/css/dataTables.colVis.css"/>
-
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head> 
-
 <div class="panel container-fluid">
-    <h1 style="font-size:20pt">Health care Patients</h1>
-
-    <h3>Person Data</h3>
     <br />
     <button class="btn btn-success" onclick="add_person()"><i class="glyphicon glyphicon-plus"></i> Add Person</button>
     <button class="btn btn-info" onclick="reload_table()"><i class="glyphicon glyphicon-refresh"></i> Reload</button>
-    <br />
-    <br />
     
-       <div class="row">
-            <div class="col-md-12">
-                <div id="colvis"></div>
-            </div>
-       </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div id="colvis"></div>
+        </div>
+    </div>
+
     <div class="table-responsive">
         <table  id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
