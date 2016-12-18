4a1b3c5f4812a5d5c786d5aea8bff41b9c17833a
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 18:41:40 +0000

    basic restyling of caregiver home

--- trunk/application/views/caregiver/caregiver_home.php
+++ trunk/application/views/caregiver/caregiver_home.php
@@ -8,29 +8,23 @@
 
 <div class=" panel container-fluid">
     </br>
+    <p class="tlScale"><?= lang('c_home_title') ?></p>
 
-    <p class="txScale"><?= lang( 'c_home_title' ) ?></p>
-
-    <p><?= lang( 'c_home_info_residents' ) ?></p>
+    <p class="txScale"><?= lang('c_home_info_residents') ?></p>
 
     <ul>
-        <?php foreach($recent_residents as $resident){
+        <?php foreach ($recent_residents as $resident) {
             ?><li> <?php echo $resident->first_name; ?></li> 
         <?php } ?>
     </ul>
-    </br>
-
-</div>
-
-<div class=" panel container-fluid">
-    </br>
+    <hr>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-6">
+            <div class="col-xs-12">
                 <div id="chart1_div" style="width:100%"></div>
             </div>
             <div class="col-xs-6">
-            </br>
+                </br>
 
             </div>
         </div>
@@ -40,57 +34,57 @@
 
 
 <script type="text/javascript">
-	
-        
-	google.charts.load('current', {'packages': ['corechart']});
-	
-        chart1function();
-        
-	//formChart1.addEventListener('submit', function(e) {
-	function chart1function() {
-		console.log('chart 1 submitted');
-		//e.preventDefault();
+
+
+    google.charts.load('current', {'packages': ['corechart']});
+
+    chart1function();
+
+    //formChart1.addEventListener('submit', function(e) {
+    function chart1function() {
+        console.log('chart 1 submitted');
+        //e.preventDefault();
 
 
 
-		$.ajax({
-			type: "POST",
-			url: "<?php echo base_url() ?>index.php/caregiver/load_avarage_score_per_category_chart",
-			data: {},
-			dataType: "text",
-			cache: false,
+        $.ajax({
+            type: "POST",
+            url: "<?php echo base_url() ?>index.php/caregiver/load_avarage_score_per_category_chart",
+            data: {},
+            dataType: "text",
+            cache: false,
 
-			success: function (data) {
-				var Yaxis = [];
-				var Xaxis = [];
-				console.log(data);
-				var response = JSON.parse(data);
-				console.log(response);
+            success: function (data) {
+                var Yaxis = [];
+                var Xaxis = [];
+                console.log(data);
+                var response = JSON.parse(data);
+                console.log(response);
 
-				Xaxis = response[0];
-				Yaxis = response[1];
+                Xaxis = response[0];
+                Yaxis = response[1];
 
-				//google.charts.load('current', {'packages':['corechart']});
-				google.charts.setOnLoadCallback(drawChart);
-
-
+                //google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
 
 
-				function drawChart() {
-
-					var data = new google.visualization.DataTable();
-					columnChart(Yaxis, Xaxis, data, "chart1_div", 'average scores of all categories');
-				}
 
 
-			}
-		});
-		return false;
-	}
-        
-        
+                function drawChart() {
 
-	
+                    var data = new google.visualization.DataTable();
+                    columnChart(Yaxis, Xaxis, data, "chart1_div", 'average scores of all categories');
+                }
+
+
+            }
+        });
+        return false;
+    }
+
+
+
+
 
 
 
