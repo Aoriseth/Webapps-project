b874d30c9c9d1772823bf2685bc11ab68d99bcb8
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 15:43:14 +0000

    better button alignment for completed page

--- application/views/resident/resident_completed.php
+++ application/views/resident/resident_completed.php
@@ -23,19 +23,18 @@
     </div>
 
     <div class="row">
-        <div class="col-xs-4">
+        <div class="pull-left">
             <form action=<?php echo base_url() . 'index.php/resident/categories' ?> method="POST">
                 <input class="btn btn-lg btn-default" type="submit" name="Categories" value="<?= lang('r_completed_start_new') ?>" style="size: 100%;color:#673AB7">
             </form>
         </div>
 
-        <div class="col-xs-4">
+        <div class="pull-right">
             <form action=<?php echo base_url() . 'index.php/resident/home' ?> method="POST">
                 <input class="btn btn-lg btn-default" type="submit" name="Categories" value="Ga naar de puzzle" style="size: 100%;color:#673AB7">
             </form>
         </div>
     </div>
-    <br>
 </div>
 
 
