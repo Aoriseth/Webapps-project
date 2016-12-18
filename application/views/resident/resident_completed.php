8f823b019544c15489d3ef9e8fed1d915b0a04d1
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 11:18:16 +0000

    added fireworks :D

--- application/views/resident/resident_completed.php
+++ application/views/resident/resident_completed.php
@@ -1,25 +1,47 @@
-<div class="container-fluid panel ">
-    <div class="container-fluid row">
-        <br>
-        <p class="tlScale">
-            <?= lang('r_completed_explanation') ?>
-        </p><hr>
-    </div>
-	<div class="tlScale tip">
+<div class="fire container-fluid panel ">
+    <div class=" ">
+        <div class="container-fluid row">
+            <br>
+            <p class="txScale">
+                <?= lang('r_completed_explanation') ?>
+            </p><hr>
+        </div>
+        <div class="tip">
+            <p class="tlScale">
+                Hier is een tip voor deze categorie:
+            </p>
             <blockquote>
-                <i>
-                    {tip}
+                <i><p class="txScale">
+                        {tip}
+                    </p>
+
                 </i>
             </blockquote>
-		
-               
-	</div> <br>
+
+
+        </div> <br>
+    </div>
+
     <div class="row">
-        <div class="col-xs-3">
+        <div class="col-xs-4">
             <form action=<?php echo base_url() . 'index.php/resident/categories' ?> method="POST">
                 <input class="btn btn-lg btn-default" type="submit" name="Categories" value="<?= lang('r_completed_start_new') ?>" style="size: 100%;color:#673AB7">
             </form>
-            <br>
+        </div>
+
+        <div class="col-xs-4">
+            <form action=<?php echo base_url() . 'index.php/resident/home' ?> method="POST">
+                <input class="btn btn-lg btn-default" type="submit" name="Categories" value="Ga naar de puzzle" style="size: 100%;color:#673AB7">
+            </form>
         </div>
     </div>
+    <br>
 </div>
+
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.fire').fireworks();
+        $('.fire').fireworks('destroy');
+    });
+</script>
