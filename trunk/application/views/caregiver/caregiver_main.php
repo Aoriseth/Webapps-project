5de0ada61f0123f9503e803c5a10029edc5fb5d2
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 21:20:41 +0000

    Reorganized caregiver_main: removed stuff that is already in include. I think there is some duplicate stuff here...

--- trunk/application/views/caregiver/caregiver_main.php
+++ trunk/application/views/caregiver/caregiver_main.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
     <head> 
-        <meta charset="utf-8">
-        
         <title><?= lang( 'c_title' ) ?></title>
-        
+
         <!--Load unique css-->
         <link href="<?php echo base_url(); ?>assets/datatables/css/dataTables.bootstrap.css" rel="stylesheet">
         <link href="<?php echo base_url(); ?>assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet">
-        
-        <!-- Load unique scripts -->
-        <script src="<?php echo base_url(); ?>assets/js/statistics/chartCallbacks.js"></script>
-        
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker3.min.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/datatables-colvis/1.1.2/css/dataTables.colVis.css"/>
+
+        <!-- Load unique scripts -->
+        <script src="<?php echo base_url(); ?>assets/js/statistics/chartCallbacks.js"></script>
+
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
         {include}
 
