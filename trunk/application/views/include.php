46adc9ea7a65582878f84983474510c5f8880298
r0366179 <r0366179@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-18 18:36:45 +0000

    added jquery ui library

--- trunk/application/views/include.php
+++ trunk/application/views/include.php
@@ -5,6 +5,7 @@
 <link rel="icon" href="<?=base_url()?>/assets/imgs/favicon.ico" type="image/gif">
 
 <!-- Load CSS -->
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/bootstrap-material-design.css">
 <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/ripples.css">
@@ -14,6 +15,7 @@
 
 <!-- Load scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="<?= base_url(); ?>assets/js/material.js"></script>
 <script src="<?= base_url(); ?>assets/js/fireworks.js"></script>
