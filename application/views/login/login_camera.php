b150bb554d8710b2750952609b4cc330754e22c3
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-21 01:49:07 +0000

    login_camera: added own error field

--- application/views/login/login_camera.php
+++ application/views/login/login_camera.php
@@ -1,11 +1,13 @@
-<?php if ( ! $this->agent->is_browser( 'Safari' ) ) { ?>
-
 <script src="<?= base_url();?>assets/js/QR-code/qcode-decoder.min.js" type="text/javascript"></script>
 <script src="<?= base_url();?>assets/js/QR-code/QRcodeRecog.js" type="text/javascript"></script>
-<script> setBaseURL("<?= base_url();?>");</script>
+<script>setBaseURL("<?= base_url();?>");</script>
 
 <p class="txScale">
 	<?= lang( 'login_camera_explanation' ) ?>
+</p>
+
+<p span style="color:red" id="error_message_camera">
+    <!-- will be used to display error messages -->
 </p>
 
 <p class="visible-sm visible-xs txScale">
@@ -15,11 +17,3 @@
 <video class="webcam" id="camfr" width="90%" height="auto" autoplay="true"></video>
     <canvas id = "canvasVideo" hidden="true" width="90%" height="auto"></canvas>
 <img id="photoFR" width="90%" height="auto">
-
-<?php } else { ?>
-
-<p>
-	Camera login is currently not supported on Safari.
-</p>
-
-<?php } ?>
