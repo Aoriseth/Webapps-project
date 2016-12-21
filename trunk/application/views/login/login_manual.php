b131657a9e86d9cbf356a61ae41829359019b909
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-21 02:04:14 +0000

    Login: split up error messages, added error messages to QR code, refactoring

--- trunk/application/views/login/login_manual.php
+++ trunk/application/views/login/login_manual.php
@@ -1,8 +1,14 @@
 <script src="<?= base_url(); ?>assets/js/login.js" type="text/javascript"></script>
 <script>
-    $(function () {
-        $("#login_form").submit(function (event) {
-            login('<?= base_url() ?>', null, null);
+    $( function () {
+        $( "#login_form" ).submit( function ( event ) {
+            login(
+                '<?= base_url() ?>',
+                $( '#login_form input[ name = username ]' ).val(),
+                $( '#login_form input[ name = password ]' ).val(),
+                $( '#error_message_manual' ),
+                false
+            );
             return false;
         });
     });
@@ -10,24 +16,25 @@
 
 <div>
     <p class="txScale">
-        <?= lang('login_manual_explanation') ?>
+        <?= lang( 'login_manual_explanation' ) ?>
     </p>
 
-    <p span style="color:red" id="error_message">
+    <p span style="color:red" id="error_message_manual">
+        <!-- will be used to display error messages -->
     </p>
 
     <form method="POST" id="login_form">
         <div class="container-fluid">
             <div class="form-group label-floating">
-                <label class="control-label" for="focusedInput1"><?= lang('login_field_username') ?></label>
+                <label class="control-label" for="focusedInput1"><?= lang( 'login_field_username' ) ?></label>
                 <input class="form-control" name="username" id="focusedInput1" required type="text">
             </div>
             <div class="form-group label-floating">
-                <label class="control-label" for="focusedInput2"><?= lang('login_field_password') ?></label>
+                <label class="control-label" for="focusedInput2"><?= lang( 'login_field_password' ) ?></label>
                 <input class="form-control" name="password" id="focusedInput2" required type="password">
             </div>
             <br>
-            <input class="btn btn-lg btn-raised btn-info" style="background-color: #673AB7;" type="submit" name="login" value="<?= lang('common_login') ?>">
+            <input class="btn btn-lg btn-raised btn-info" style="background-color: #673AB7;" type="submit" name="login" value="<?= lang( 'common_login' ) ?>">
         </div>
     </form>
 </div>
--- trunk/assets/js/login.js
+++ trunk/assets/js/login.js
@@ -1,50 +1,42 @@
-var videoBoolean = null;
-function login(base_url, usernameI, passwordI) {
-    console.log("usernameI" + usernameI + "passwordI" + passwordI + " videoBoolean   " + videoBoolean);
-    var username = usernameI;
-    var password = passwordI;
+function login( base_url, username, password, error_feedback, from_camera ) {
 
     var url = base_url + 'index.php/login/ajax';
-    if (username === null || password === null) {
-        console.log("inside if loop");
-        username = $('#login_form input[ name = username ]').val();
-        password = $('#login_form input[ name = password ]').val();
-        videoBoolean = 'false';
-    } else {
-        videoBoolean = 'true';
-    }
-    console.log("videoBoolean " + videoBoolean);
+    var videoBoolean = ( from_camera ? 'true' : 'false' );
+
+    console.log( 'login attempt: ' + username + ' / ' + password + ( from_camera ? ' from camera' : '' ) )
+
     $.ajax({
         type: "POST",
         url: url,
         data: {
             "username": username,
             "password": password,
-            "video":videoBoolean
+            "video": videoBoolean
         },
         dataType: "text",
         cache: false,
     })
-            .done(function (data) {
-                console.log("data: " + data);
-                
-                   
-                var response = JSON.parse(data);
-         
-                console.log("response:  " + JSON.stringify(response));
-                if (response[ 'success' ]) {
-                    // redirect to home page
-                    window.location = base_url;
-                } else {
-                    // display message what's wrong
-                    $('#error_message').html(response[ 'error' ]);
-                    // return focus to username
+        .done( function ( data ) {
+            var response = JSON.parse( data );
+            console.log( 'AJAX response: ' + JSON.stringify( response ) );
+
+            if (response[ 'success' ]) {
+                console.log( 'login success' );
+                // redirect to home page
+                window.location = base_url;
+            } else {
+                console.log( 'login failed: ' + response[ 'error' ] );
+                // display what's wrong
+                error_feedback.html(response[ 'error' ]);
+                // return focus to username
+                if ( ! from_camera ) {
                     $('#login_form input[ name = username ]').focus();
                 }
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                // display error message
-                $('#error_message').html('Error connecting to server: ' + errorThrown);
-            });
+            }
+        })
+        .fail( function ( jqXHR, textStatus, errorThrown ) {
+            console.log( 'AJAX error: ' + errorThrown );
+            // display error message
+            error_feedback.html( 'Error connecting to server: ' + errorThrown );
+        });
 }
-
