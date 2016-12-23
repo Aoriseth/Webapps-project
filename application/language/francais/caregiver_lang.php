28a66e12cf5f8729202b914cf3976ab420fc3b2c
r0652164 <r0652164@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2016-12-23 09:05:59 +0000

    French translation

--- application/language/francais/caregiver_lang.php
+++ application/language/francais/caregiver_lang.php
@@ -11,44 +11,44 @@ $lang[ 'c_navbar_statistics' ] = 'Statistiques';
 
 // home
 $lang[ 'c_home_title' ] = 'Bonjour {name}!';
-$lang[ 'c_home_info_residents' ] = 'The following residents most recently completed their questionnaire:';
+$lang[ 'c_home_info_residents' ] = 'Voici les residents ayant fini un questionnaire récemment:';
 
 // overview
-$lang[ 'c_overview_add_person' ] = 'Add Person';
-$lang[ 'c_overview_reload' ] = 'Reload';
+$lang[ 'c_overview_add_person' ] = 'Ajouter un Personne';
+$lang[ 'c_overview_reload' ] = 'Actualiser';
 
 $lang[ 'c_overview_id' ] = 'ID';
-$lang[ 'c_overview_first_name' ] = 'First Name';
-$lang[ 'c_overview_last_name' ] = 'Last Name';
-$lang[ 'c_overview_gender' ] = 'Gender';
-$lang[ 'c_overview_gender_male' ] = 'Male';
-$lang[ 'c_overview_gender_female' ] = 'Female';
-$lang[ 'c_overview_gender_other' ] = 'Other';
-$lang[ 'c_overview_password' ] = 'Password';
-$lang[ 'c_overview_date_of_birth' ] = 'Date of Birth';
-$lang[ 'c_overview_language' ] = 'Language';
-$lang[ 'c_overview_language_english' ] = 'English';
+$lang[ 'c_overview_first_name' ] = 'Prenom';
+$lang[ 'c_overview_last_name' ] = 'Nom';
+$lang[ 'c_overview_gender' ] = 'Sexe';
+$lang[ 'c_overview_gender_male' ] = 'Homme';
+$lang[ 'c_overview_gender_female' ] = 'Femme';
+$lang[ 'c_overview_gender_other' ] = 'Autre';
+$lang[ 'c_overview_password' ] = 'Mot de passe';
+$lang[ 'c_overview_date_of_birth' ] = 'Date de naissance';
+$lang[ 'c_overview_language' ] = 'Langue';
+$lang[ 'c_overview_language_english' ] = 'Anglais';
 $lang[ 'c_overview_language_francais' ] = 'Francais';
-$lang[ 'c_overview_language_nederlands' ] = 'Nederlands';
-$lang[ 'c_overview_floor' ] = 'Floor';
-$lang[ 'c_overview_room' ] = 'Room';
-$lang[ 'c_overview_last_activity' ] = 'Last activity';
-$lang[ 'c_overview_last_completed_session' ] = 'Last completed session';
-$lang[ 'c_overview_amount_of_sessions' ] = 'Completed sessions';
+$lang[ 'c_overview_language_nederlands' ] = 'Néerlandais';
+$lang[ 'c_overview_floor' ] = 'Etage';
+$lang[ 'c_overview_room' ] = 'Chambre';
+$lang[ 'c_overview_last_activity' ] = 'Dernière connexion';
+$lang[ 'c_overview_last_completed_session' ] = 'Derniere session finie';
+$lang[ 'c_overview_amount_of_sessions' ] = 'Sessions finies';
 $lang[ 'c_overview_domicile' ] = 'Domicile';
-$lang[ 'c_overview_type' ] = 'Type of account';
+$lang[ 'c_overview_type' ] = 'Type de compte';
 $lang[ 'c_overview_type_resident' ] = 'Resident';
-$lang[ 'c_overview_type_caregiver' ] = 'Caregiver';
+$lang[ 'c_overview_type_caregiver' ] = 'Soignant';
 
 // statistics
-$lang[ 'c_statistics_filter' ] = 'Filter';
-$lang[ 'c_statistics_gender' ] = 'Genre';
-$lang[ 'c_statistics_male' ] = 'Mâle';
-$lang[ 'c_statistics_female' ] = 'Femelle';
+$lang[ 'c_statistics_filter' ] = 'Filtres';
+$lang[ 'c_statistics_gender' ] = 'Sexe';
+$lang[ 'c_statistics_male' ] = 'Homme';
+$lang[ 'c_statistics_female' ] = 'Femme';
 $lang[ 'c_statistics_floor' ] = 'Étage';
 $lang[ 'c_statistics_age' ] = 'Âge';
 $lang[ 'c_statistics_result' ] = 'Résultat';
 $lang[ 'c_statistics_no_result' ] = 'Aucun résultat, modifier filtre.';
-$lang[ 'c_statistics_all_category_selected_individual' ] = 'Tous scores des catégories de résidents sélectionnés';
-$lang[ 'c_statistics_all_category_individual' ] = 'Tous scores des catégories par résident';
+$lang[ 'c_statistics_all_category_selected_individual' ] = 'Score des différentes catégories pour les résidents sélectionnés';
+$lang[ 'c_statistics_all_category_individual' ] = 'Scores des différentes catégories par résident';
 $lang[ 'c_statistics_category_all_individual' ] = 'Tous les résidents par catégories';
