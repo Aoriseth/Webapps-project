55d75560c5b7a3f87cd98a9019568209245d94ea
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2017-01-25 18:04:34 +0000

    local load of jquerry and responsiveslides

--- application/views/resident/resident_gallery.php
+++ application/views/resident/resident_gallery.php
@@ -1,6 +1,5 @@
 <!-- Load  Unique scripts -->
-        <script src="<?php echo base_url(); ?>assets/js/puzzle.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/responsiveslides.js"></script>
 <div data-placement="bottom" data-toggle="popover" title="" data-container="body" data-content="<?= lang('r_gallery_help') ?>" class="popup panel container-fluid">
 
