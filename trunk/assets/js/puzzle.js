5cdc65794bf4b1bfa6e19a95851f452243550731
r0368306 <r0368306@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2017-01-26 00:26:33 +0000

    canvas ipv images in ul. allows for better ratios and more flexibility

--- trunk/assets/js/puzzle.js
+++ trunk/assets/js/puzzle.js
@@ -1,3 +1,5 @@
+
+//resizes canvas and draws the pieces according to the array of booleans in 'categories'
 function loadPuzzle(base_url, path, name, categories) {
 
     var image = new Image();
@@ -12,36 +14,37 @@ function loadPuzzle(base_url, path, name, categories) {
         greyImage2.src = base_url.concat('/assets/imgs/673ab7.png');
         greyImage.src = base_url.concat('/assets/imgs/question-mark12.png');
     };
-
     greyImage.onload = function () {
+
+        //resizing the canvas and calculate ratios
         var canvas = document.getElementById("puzzle");
         var context = canvas.getContext("2d");
         context.imageSmoothingEnabled = false;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight * 0.8;
-        var hRatio = canvas.width / image.width;
-        var vRatio = canvas.height / image.height;
-
+        var cheight = canvas.height;
+        var cwidth = canvas.width;
+        var iheight = image.height;
+        var iwidth = image.width;
         for (var y = 0; y < 4; ++y) {
             for (var x = 0; x < 3; ++x) {
-                if (x === 0 && y === 0) {
-                    context.drawImage(image, x * (image.width / 3), y * (image.height / 4), (image.width / 3), (image.height / 4),
-                            (x * image.width * hRatio) / 3, (y * image.height * vRatio) / 4, (image.width * hRatio) / 3, (image.height * vRatio) / 4);
-                } else if (categories[3 * y + x - 1] === 1) {
-                    context.drawImage(image, x * (image.width / 3), y * (image.height / 4), (image.width / 3), (image.height / 4),
-                            (x * image.width * hRatio) / 3, (y * image.height * vRatio) / 4, (image.width * hRatio) / 3, (image.height * vRatio) / 4);
-                    //console.log("piece drawn" + y + x);
+                if (x === 0 && y === 0 || categories[3 * y + x - 1] === 1) {
+                    //first piece is always drawn, then it depends on the array 'categories'
+                    context.drawImage(image, x * (iwidth / 3), y * (iheight / 4), (iwidth / 3), (iheight / 4),
+                            (x * cwidth) / 3, (y * cheight) / 4, (cwidth) / 3, (cheight) / 4);
                 } else {
+                    //else purple piece with questionmark is drawn
                     context.drawImage(greyImage2,
-                            (x * image.width * hRatio) / 3, (y * image.height * vRatio) / 4, (image.width * hRatio) / 3, (image.height * vRatio) / 4);
+                            (x * cwidth) / 3, (y * cheight) / 4, (cwidth) / 3, (cheight) / 4);
                     context.drawImage(greyImage,
-                            (x * image.width * hRatio) / 3, (y * image.height * vRatio) / 4, (image.width * hRatio) / 3, (image.height * vRatio) / 4);
+                            (x * cwidth) / 3, (y * cheight) / 4, (cwidth) / 3, (cheight) / 4);
                 }
             }
         }
     };
 }
 
+//resizes the canvas, calculates what piece was collected and draws it.
 function loadPuzzlePiece(base_url, path, name, setID) {
     var image = new Image();
     if (path !== null && name !== null) {
@@ -50,35 +53,45 @@ function loadPuzzlePiece(base_url, path, name, setID) {
         image.src = base_url + "/assets/imgs/reserve.png";
     }
     (image.onload) = function () {
+        //resize canvas
         var canvas = document.getElementById("puzzle");
         var context = canvas.getContext("2d");
         context.imageSmoothingEnabled = false;
         canvas.width = window.innerWidth / 3;
         canvas.height = window.innerHeight / 4;
-
+        
+        //calculate the x and y according to the setID.
         var x = ((parseInt(setID)) % 3);
         var y = ((parseInt(setID) - x) / 3);
-
         context.drawImage(image, x * (image.width / 3), y * (image.height / 4), (image.width / 3), (image.height / 4), 0, 0, canvas.width, canvas.height);
     };
-
-
 }
 
-
+//creates and resizes the canvases that contain all images.
+//sets the images in the canvasses.
 function loadGallery(base_url, data) {
 
-// now get the first element:
-
     if (data !== null) {
+
         document.getElementById("slider").width = window.outerWidth;
         $('#slider').empty();
         for (var i = 0; i < data.length; i++) {
             var imgName = "slide" + i.toString();
-            $('#slider').append('<li id="centered-btns1_s0" style="display: block; float: none; position: absolute; opacity: 0; z-index: 2; transition: opacity 500ms ease-in-out 0s;" class="img-responsive" ><img class="img-responsive" id="' + imgName + '"></li>');
-
-            document.getElementById(imgName).src = base_url + data[i]['picture_dir'] + data[i]['picture_name'];
-            document.getElementById(imgName).width = document.getElementById("slider").width;
+            $('#slider').append('<li style="display: block; float: none; position: absolute; opacity: 0; z-index: 2; transition: opacity 500ms ease-in-out 0s;"><canvas class="img-responsive" id="' + imgName + '"></canvas></li>');
+            var canvas = document.getElementById(imgName);
+            var context = canvas.getContext("2d");
+            context.imageSmoothingEnabled = false;
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            var image = new Image();
+            image.src = base_url + data[i]['picture_dir'] + data[i]['picture_name'];
+            //this is an inner closure that creates an executable anonymous function. If this would not be done,
+            //only the last image would be drawn.
+            image.onload = (function (localCtx, localImg) {
+                return function () {
+                    localCtx.drawImage(localImg, 0, 0, canvas.width, canvas.height);
+                }
+            })(context, image);
         }
     } else {
         document.getElementById("no_pictures_message").style.visibility = 'visible';
