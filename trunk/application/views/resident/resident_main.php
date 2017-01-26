bc9209ccb2c3c36cbf44fb88891ed26d02adbb05
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2017-01-26 01:10:05 +0000

    Fix HTML on resident main page according to validator.

--- trunk/application/views/resident/resident_main.php
+++ trunk/application/views/resident/resident_main.php
@@ -1,55 +1,54 @@
 <!DOCTYPE html>
+
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <title><?= lang('r_title') ?></title>
+        <title><?= lang( 'r_title' ) ?></title>
 
         <!--Include common scripts and css-->
         {include}
-
-        
-
 
     </head>
     <body>
         <!--Navigation bar of this page-->
         {navbar}
-        <br><br>
+        <br>
+        <br>
+
         <!-- the main element of the page (puzzle, etc) -->
         <div class="container" id="container_resident">
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
                     <div>
+
                         {content}	
+
                     </div> 
                 </div>
             </div>
         </div>
 
-        <!--Help button for resident pages-->
-        <!--        <button id="trigger" class="btn btn-warning" style="height:3em;width:5em;position:fixed;top:60px;left:10px;">
-        <?= lang('r_button_help') ?>
-                </button>-->
+        <!-- Javascript -->
+        <script>
+            $( document ).ready( function () {
+                $( '#trigger' ).click( function ( event ) {
+                    $( '.popup' ).popover( 'toggle' );
+                    event.stopPropagation();
+                });
+
+//              $( '#trigger' ).click( function ( event ) {
+//                  $( '.important' ).effect( 'shake', 1500 );
+//                  $( ".important" ).dialog( { modal: true } );
+//              });
+
+                $( window ).click( function () {
+                    // Hide the menus if visible
+                    $( '.popup' ).popover( 'hide' );
+                });
+            });
+        </script>
+        <script>$.material.init();</script>
+
     </body>
+
 </html>
 
-<!-- Javascripting-->
-<script>
-    $(document).ready(function () {
-        $('#trigger').click(function (event) {
-            $('.popup').popover('toggle');
-            event.stopPropagation();
-        });
-
-//        $('#trigger').click(function (event) {
-//            $('.important').effect('shake',1500);
-//            $( ".important" ).dialog({ modal: true });
-//        });
-
-        $(window).click(function () {
-            //Hide the menus if visible
-            $('.popup').popover('hide');
-        });
-    });
-</script>
-<script>$.material.init();</script>
