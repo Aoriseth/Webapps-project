29882f95d29c9f105ab6142d14de589847e349d3
r0373169 <r0373169@ca9ba379-d244-4407-ab6d-dce1c7daeeec>
2017-01-26 16:46:36 +0000

    Fix HTML according to validator: move {include} around and move <script> inside <body>

--- trunk/application/views/caregiver/caregiver_main.php
+++ trunk/application/views/caregiver/caregiver_main.php
@@ -3,6 +3,9 @@
     <head> 
         <title><?= lang('c_title') ?></title>
 
+        <!--Include common scripts and css-->
+        {include}
+
         <!--Load unique css-->
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/datatables/css/dataTables.bootstrap.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css">
@@ -15,26 +18,25 @@
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
-        {include}
 
-        <!--	HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--	 WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--	[if lt IE 9]>
-                          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-                          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-                        <![endif]-->
     </head> 
     <body>
+
         {navbar}
-        <br><br>
+        <br>
+        <br>
+        
         <div class="row">
             <div class="col-md-10 col-md-offset-1 col-sm-12 col-sm-offset-0">
+                
                     {content}
                 
             </div>
         </div>
+
+        <!--Activate material design-->
+        <script>$.material.init();</script>
+
     </body>
 </html>
 
-<!--Activate material design-->
-<script>$.material.init();</script>
